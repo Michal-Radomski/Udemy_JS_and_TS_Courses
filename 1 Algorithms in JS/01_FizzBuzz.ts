98668fd51295bc63232e0a59287cc075e1c9f0c5
Michal Radomski <michalra@op.pl>
2023-01-08 15:15:18 +0100

    Exercise 01 done - fixed

--- 1 Algorithms in JS/01_FizzBuzz.ts
+++ 1 Algorithms in JS/01_FizzBuzz.ts
@@ -1,11 +1,13 @@
 function fizzBuzz(num: number) {
   for (let i = 1; i <= num; i++) {
+    // if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    }
     if (i % 3 === 0) {
       console.log("Fizz");
     } else if (i % 5 === 0) {
       console.log("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
     } else {
       console.log(i);
     }

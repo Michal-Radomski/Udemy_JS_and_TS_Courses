b285e424e1dde524942a26b7185e662237f75cc9
Michal Radomski <michalra@op.pl>
2023-01-08 17:33:57 +0100

    Exercise 03 done

--- 1 Algorithms in JS/index.ts
+++ 1 Algorithms in JS/index.ts
@@ -18,35 +18,60 @@
 // fizzBuzz(31);
 
 //* 02 Harmless Ransom Note Algorithm
-function harmlessRansomNote(noteText: string, magazineText: string) {
-  let noteArray = noteText.split(" ");
-  let magazineArray = magazineText.split(" ");
+// function harmlessRansomNote(noteText: string, magazineText: string) {
+//   let noteArray = noteText.split(" ");
+//   let magazineArray = magazineText.split(" ");
 
-  let magazineObj: { [word: string]: number } = {};
+//   let magazineObj: { [word: string]: number } = {};
 
-  magazineArray.forEach((word) => {
-    if (!magazineObj[word]) {
-      magazineObj[word] = 0;
+//   magazineArray.forEach((word) => {
+//     if (!magazineObj[word]) {
+//       magazineObj[word] = 0;
+//     }
+//     magazineObj[word]++;
+//   });
+//   // console.log("magazineObj:", magazineObj);
+
+//   let noteIsPossible = true;
+//   noteArray.forEach((word) => {
+//     if (magazineObj[word]) {
+//       magazineObj[word]--;
+//       if (magazineObj[word] < 0) {
+//         noteIsPossible = false;
+//       }
+//     } else noteIsPossible = false;
+//   });
+//   console.log("noteIsPossible:", noteIsPossible);
+//   return noteIsPossible;
+// }
+
+// harmlessRansomNote("this this", "this is all the magazine text in the magazine");
+// harmlessRansomNote(
+//   "this is a secret note for you from a secret admirer",
+//   "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
+// );
+
+//* 03 isPalindrome Algorithm
+function isPalindrome(string: string) {
+  string = string.toLowerCase();
+  let charactersArray = string.split("");
+  let validCharacters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  let lettersArray: string[] = [];
+
+  charactersArray.forEach((character) => {
+    if (validCharacters.indexOf(character) > -1) {
+      lettersArray.push(character);
     }
-    magazineObj[word]++;
   });
-  // console.log("magazineObj:", magazineObj);
+  console.log("lettersArray:", lettersArray);
 
-  let noteIsPossible = true;
-  noteArray.forEach((word) => {
-    if (magazineObj[word]) {
-      magazineObj[word]--;
-      if (magazineObj[word] < 0) {
-        noteIsPossible = false;
-      }
-    } else noteIsPossible = false;
-  });
-  console.log("noteIsPossible:", noteIsPossible);
-  return noteIsPossible;
+  if (lettersArray.join("") === lettersArray.reverse().join("")) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
 }
-
-harmlessRansomNote("this this", "this is all the magazine text in the magazine");
-harmlessRansomNote(
-  "this is a secret note for you from a secret admirer",
-  "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited"
-);
+isPalindrome("Madam I'm Adam");

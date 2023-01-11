c7f4c514f3177980a5c2d7e1d2a00901835f5bd9
Michal Radomski <michalra@op.pl>
2023-01-12 00:32:22 +0100

    Exercise 13 done

--- 1 Algorithms in JS/index.ts
+++ 1 Algorithms in JS/index.ts
@@ -315,3 +315,17 @@
 // sieveOfEratosthenes(49);
 
 //* 13 Buble Sort Algorithm
+function bubbleSort(numArray: Array<number>): Array<number> {
+  for (let i = numArray.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (numArray[j] > numArray[j + 1]) {
+        let temp = numArray[j];
+        numArray[j] = numArray[j + 1];
+        numArray[j + 1] = temp;
+      }
+    }
+  }
+  return numArray;
+}
+
+console.log(bubbleSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 9, 1]));

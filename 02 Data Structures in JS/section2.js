a1647fb38218f2557b2cee6b779087bc6ddd0c60
Michal Radomski <michalra@op.pl>
2023-01-14 13:13:41 +0100

    Linked List - commit2

--- 02 Data Structures in JS/section2.js
+++ 02 Data Structures in JS/section2.js
@@ -11,7 +11,25 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
-const LL = new LinkedList();
-console.log("LL:", LL);
-const node1 = new Node(100, "node2", null);
-console.log("node1:", node1);
+// const LL = new LinkedList();
+// console.log("LL:", LL);
+// const node1 = new Node(100, "node2", null);
+// console.log("node1:", node1);
+
+LinkedList.prototype.addToHead = function (value) {
+  let newNode = new Node(value, this.head, null);
+
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  this.head = newNode;
+};
+
+const ll = new LinkedList();
+console.log("ll:", ll);
+ll.addToHead(100);
+ll.addToHead(200);
+ll.addToHead(300);
+console.log("ll:", ll);

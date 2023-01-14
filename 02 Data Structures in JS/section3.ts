d2b67a21ec275c3492f75bd73e773cdd6a04eead
Michal Radomski <michalra@op.pl>
2023-01-15 00:37:38 +0100

    Section 3 - commit2

--- 02 Data Structures in JS/section3.ts
+++ 02 Data Structures in JS/section3.ts
@@ -28,6 +28,27 @@ BST.prototype.insert = function (value: number) {
   }
 };
 
+BST.prototype.contains = function (value: number) {
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else if (value > this.value) {
+    if (!this.right) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
+};
+
 const bst = new (BST as any)(50);
 bst.insert(30);
-console.log("bst:", bst, typeof bst);
+bst.insert(50);
+bst.insert(10);
+// console.log("bst:", bst, typeof bst);
+console.log(bst.contains(50));

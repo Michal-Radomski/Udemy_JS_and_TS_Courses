276acf20b07922ba7d39518bb0ed1d7895798aaf
Michal Radomski <michalra@op.pl>
2023-01-15 14:31:05 +0100

    Section 4 - commit3

--- 02 Data Structures in JS/section4.ts
+++ 02 Data Structures in JS/section4.ts
@@ -41,5 +41,35 @@ HashTable.prototype.hash = function (key: string) {
   return bucket;
 };
 
+// const myHT = new (HashTable as any)(30);
+// console.log(myHT.hash("Rebecca"));
+
+HashTable.prototype.insert = function (key: string, value: string) {
+  const index = this.hash(key);
+  console.log({ index });
+  if (!this.buckets[index]) {
+    this.buckets[index] = new (HashNode as any)(key, value);
+  } else if (this.buckets[index].key === key) {
+    this.buckets[index].value = value;
+  } else {
+    let currentNode = this.buckets[index];
+    while (currentNode.next) {
+      if (currentNode.next.key === key) {
+        currentNode.next.value = value;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new (HashNode as any)(key, value);
+  }
+};
+
 const myHT = new (HashTable as any)(30);
-console.log(myHT.hash("Rebecca"));
+myHT.insert("Dean", "dean@gmail.com");
+myHT.insert("Megan", "megan@gmail.com");
+myHT.insert("Dane", "dane@yahoo.com");
+myHT.insert("Dean", "deanmachine@gmail.com");
+myHT.insert("Megan", "megansmith@gmail.com");
+myHT.insert("Dane", "dane1010@outlook.com");
+// console.log("myHT:", myHT, typeof myHT);
+console.log("myHT.buckets:", myHT.buckets);

a104b12fe785f9ab3405e32da86508056ff93e1e
Michal Radomski <michalra@op.pl>
2023-08-29 02:05:39 +0200

    JS ES6 course - commit 02

--- 03 ES6 JS/index.ts
+++ 03 ES6 JS/index.ts
@@ -1,3 +1,4 @@
+//* Nested forEach loop
 const datesArray = ["2019-01-11", "2019-01-12", "2019-01-13", "2019-01-14"] as string[];
 const timesArray = ["00:00:00", "06:00:00", "12:00:00", "18:00:00"] as string[];
 const datesTimesArray = [] as string[];
@@ -13,7 +14,8 @@ console.log("datesTimesArray:", datesTimesArray);
 
 //* 01 forEach
 // Old way - for loop
-var colors = ["red", "blue", "green"];
-for (var i = 0; i < colors.length; i++) {
-  console.log(colors[i]);
-}
+const colors = ["red", "blue", "green"];
+// for (var i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// }
+colors.forEach((color) => console.log(color));

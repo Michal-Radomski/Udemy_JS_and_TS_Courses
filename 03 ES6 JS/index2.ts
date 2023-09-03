4f3da10ac503a0960ebf94955f5519fab23bd8e9
Michal Radomski <michalra@op.pl>
2023-09-04 00:03:47 +0200

    JS ES6 course - commit 42

--- 03 ES6 JS/index2.ts
+++ 03 ES6 JS/index2.ts
@@ -438,4 +438,52 @@ console.log("$.ajax:", $.ajax);
   const myColors = [];
   for (let color of colors2()) myColors.push(color);
   console.log("myColors", myColors);
+
+  interface TestingTeam {
+    lead: string;
+    tester: string;
+  }
+
+  interface EngineeringTeam {
+    size: number;
+    department: string;
+    lead: string;
+    manager: string;
+    engineering: string;
+    testingTeam: TestingTeam;
+  }
+
+  const testingTeam = {
+    lead: "Amanda",
+    tester: "Bill",
+  };
+
+  const engineeringTeam: EngineeringTeam = {
+    testingTeam: testingTeam,
+    size: 3,
+    department: "Engineering",
+    lead: "Jill",
+    manager: "Alex",
+    engineering: "Dave",
+  };
+
+  function* TestingTeamIterator(team: TestingTeam) {
+    yield team.lead;
+    yield team.tester;
+  }
+
+  function* TeamIterator(team: EngineeringTeam) {
+    yield team.lead;
+    yield team.manager;
+    yield team.engineering;
+    const testingTeamGenerator = TestingTeamIterator(team.testingTeam);
+    // console.log("testingTeamGenerator:", testingTeamGenerator);
+    yield* testingTeamGenerator;
+  }
+
+  const names = [] as string[];
+  for (let name of TeamIterator(engineeringTeam)) {
+    names.push(name);
+  }
+  console.log({ names });
 }

f585a84a5cf8473a485d362dc26cce0cea4717fe
Michal Radomski <michalra@op.pl>
2023-09-06 02:06:46 +0200

    JS OOP - commit 04

--- 04 JS OOP/src/index.ts
+++ 04 JS OOP/src/index.ts
@@ -24,3 +24,34 @@ const Funcs = {
 };
 Funcs.derrMethod(Funcs.funArr, "kwadratowa", "Pochodna");
 Funcs.derrMethod(Funcs.funArr, "liniowa", "Pochodna2");
+
+const obj = {
+  funEx1: "2x^2+4x+3",
+  funEx2: "",
+  afactor: 2,
+  bfactor: 4,
+  cfactor: 3,
+  deltaMethod: function (f: any) {
+    let a = f.slice(0, -8);
+    a = parseInt(a);
+    let b = f.slice(5, -3);
+    b = parseInt(b);
+    let c = f.slice(8);
+    c = parseInt(c);
+    // console.log("this:", this);
+    // console.log("obj:", obj);
+    console.log("this === obj:", this === obj);
+    if (a == this.afactor && b == this.bfactor && c == this.cfactor) {
+      document.getElementById("Answer")!.innerHTML = "Współczynniki się zgadzają !!!";
+      let delta = Math.pow(b, 2) - 4 * a * c;
+      document.getElementById("Delta")!.innerHTML = "Delta jest równa: " + delta;
+    } else {
+      document.getElementById("Answer")!.innerHTML = "Współczynniki się nie zgadzają !!!";
+      document.getElementById("Delta")!.innerHTML = "Zatem jak na złość nie będzie też Delty !!!";
+    }
+  },
+};
+
+// obj.funEx1 = "2x^2+4x+3";
+obj.funEx2 = "3x^2+5x+4";
+obj.deltaMethod(obj.funEx1);

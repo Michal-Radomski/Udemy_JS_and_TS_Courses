72e5fd7ac9a41897049f9893f6abb825defe94a4
Michal Radomski <michalra@op.pl>
2023-09-17 23:50:58 +0200

    Course - commit 02

--- 05 ES6, ES7, ES8, EcmaScript/01/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/01/index.ts
@@ -1,4 +1,5 @@
-"use script";
+export {};
+("use script");
 // var i = 2;
 // console.log({ i });
 
@@ -22,15 +23,49 @@
 // console.log({ k });
 // // k=3 //* Error
 
-const myArray = [1, 2, 3, 4] as number[];
-console.log({ myArray });
-myArray.push(5);
-console.log({ myArray });
+// const myArray = [1, 2, 3, 4] as number[];
+// console.log({ myArray });
+// myArray.push(5);
+// console.log({ myArray });
 
-const obj = {
-  name: "Rob",
-  career: "Teacher",
-} as { name: string; career: string; gender?: string };
+// const obj = {
+//   name: "Rob",
+//   career: "Teacher",
+// } as { name: string; career: string; gender?: string };
 
-obj.gender = "male";
-console.log({ obj });
+// obj.gender = "male";
+// console.log({ obj });
+
+// Old way:
+// const stuffFromAPI = {
+//   name: "Rob",
+//   career: "Teacher",
+//   language: "JavaScript",
+// };
+
+// const name = stuffFromAPI.name;
+// const career = stuffFromAPI.career;
+// const language = stuffFromAPI.language;
+
+// const newThingToPassToAPI = {
+//   name: name,
+//   career: career,
+//   language: language,
+// };
+// console.log({ newThingToPassToAPI });
+
+// New way
+// const coolNewWay = {
+//   name,
+//   career,
+//   language,
+// };
+// console.log({ coolNewWay });
+
+// Multiline
+// const string = "text" + "text2";
+// const string = "text \n text2";
+// console.log({ string });
+// const string = `text1
+// text2`;
+// console.log({ string });

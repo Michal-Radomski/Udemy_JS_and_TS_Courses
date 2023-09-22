584da3267331aa12f7f0043c1e5b1c82040cffb5
Michal Radomski <michalra@op.pl>
2023-09-23 00:48:23 +0200

    Course - commit 13

--- 05 ES6, ES7, ES8, EcmaScript/02/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/02/index.ts
@@ -1,23 +1,46 @@
 //* Classes
 // The old way
-function SuperHero(this: any, name: string, strength: number, speed: number, weapon: string, cape: boolean) {
-  this.name = name;
-  this.strength = strength;
-  this.speed = speed;
-  this.weapon = weapon;
-  this.cape = cape;
-  // this.goodHero = true;
-  // this.powerUp = function(){
-  // 	this.strength += 5;
-  // }
+// function SuperHero(this: any, name: string, strength: number, speed: number, weapon: string, cape: boolean):void {
+//   this.name = name;
+//   this.strength = strength;
+//   this.speed = speed;
+//   this.weapon = weapon;
+//   this.cape = cape;
+//   // this.goodHero = true;
+//   // this.powerUp = function(){
+//   // 	this.strength += 5;
+//   // }
+// }
+
+// SuperHero.prototype.goodHero = true;
+// SuperHero.prototype.powerUp = function () {
+//   this.strength += 5;
+// };
+
+// const hero1 = new (SuperHero as any)("Hank", 10, 5, "Fist", true);
+// hero1.name = "Ed";
+// hero1.powerUp();
+// console.log({ hero1 }, typeof hero1);
+
+// ES6
+class SuperHero {
+  name: string;
+  strength: number;
+  speed: number;
+  weapon: string;
+  cape: boolean;
+  constructor(name: string, strength: number, speed: number, weapon: string, cape: boolean) {
+    this.name = name;
+    this.strength = strength;
+    this.speed = speed;
+    this.weapon = weapon;
+    this.cape = cape;
+  }
+  powerUp() {
+    this.strength += 5;
+  }
 }
 
-SuperHero.prototype.goodHero = true;
-SuperHero.prototype.powerUp = function () {
-  this.strength += 5;
-};
-
-let hero1 = new (SuperHero as any)("Hank", 10, 5, "Fist", true);
-hero1.name = "Ed";
-hero1.powerUp();
-console.log({ hero1 });
+const hero1: SuperHero = new SuperHero("Hank", 10, 5, "Fist", true);
+// hero1.name = "Ed";
+console.log({ hero1 }, typeof hero1);

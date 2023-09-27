cb1a3e2fd2ba6241f0aa93afe8c73db02566b0f9
Michal Radomski <michalra@op.pl>
2023-09-28 00:26:32 +0200

    Course - commit 30

--- 05 ES6, ES7, ES8, EcmaScript/03/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/03/index.ts
@@ -1,10 +1,30 @@
-setTimeout(() => {
-  console.log("Line 1, 0 timeout -> 3"); //* 3
-}, 0);
-console.log("line 2 -> 1"); //* 1
-setTimeout(() => console.log("Line 3, 100 timeout -> 4"), 100); //* 4
-for (let i = 0; i < 100001; i++) {
-  if (i === 100000) {
-    console.log("Done with loop -> 2"); //* 2
-  }
+// setTimeout(() => {
+//   console.log("Line 1, 0 timeout -> 3"); //* 3
+// }, 0);
+// console.log("line 2 -> 1"); //* 1
+// setTimeout(() => console.log("Line 3, 100 timeout -> 4"), 100); //* 4
+// for (let i = 0; i < 100001; i++) {
+//   if (i === 100000) {
+//     console.log("Done with loop -> 2"); //* 2
+//   }
+// }
+
+//* Callback
+const printUpper = function (text: string) {
+  console.log(text.toUpperCase());
+};
+
+const printNumber = function (number: number) {
+  console.log(number);
+};
+
+function run(callback: Function, input: string | number) {
+  callback(input);
 }
+
+run(printUpper, "Test");
+run(printNumber, 5);
+
+run((text: string) => {
+  console.log(text.toUpperCase());
+}, `Hello, World`);

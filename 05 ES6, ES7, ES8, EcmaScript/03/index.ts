0d0e89ea653cef8b118daeb10b6af7a06a8cc46a
Michal Radomski <michalra@op.pl>
2023-09-28 01:49:58 +0200

    Course - commit 32

--- 05 ES6, ES7, ES8, EcmaScript/03/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/03/index.ts
@@ -30,3 +30,67 @@
 // }, `Hello, World`);
 
 //* Promise
+// const myFirstPromise = new Promise((resolve, _reject) => {
+//   console.log("Hey, I'm running!");
+//   console.log("Hey, I'm also running!");
+// //* ajax, axios...
+//   const lang = ["JS", "Java", "Go"];
+//   // reject("I did not finish"); //Something went wrong!
+//   resolve(lang); // Im DONE!
+// });
+// console.log(myFirstPromise);
+
+// myFirstPromise.then((promiseData) => {
+//   console.log(promiseData);
+// });
+// myFirstPromise.catch((promiseError) => {
+//   console.log(promiseError);
+// });
+// console.log("=====================");
+// console.log("I'm the last line in the program!");
+// console.log("=====================");
+
+function one() {
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve("2 seconds has passed");
+    }, 2000);
+  });
+}
+
+function two() {
+  return new Promise((resolve, _reject) => {
+    setTimeout(() => {
+      resolve("1 second has passed");
+    }, 1000);
+  });
+}
+
+const promiseOne = one();
+const promiseTwo = two();
+promiseOne.then((msgFromPromise) => {
+  console.log("=====1====");
+  console.log(1, { msgFromPromise });
+  console.log(2, { promiseOne });
+});
+promiseTwo.then((msgFromPromise) => {
+  console.log("=====2====");
+  console.log(3, { msgFromPromise });
+  console.log(4, { promiseTwo });
+});
+
+console.log(5, { promiseOne });
+console.log(6, { promiseTwo });
+
+const promiseArray = [promiseOne, promiseTwo];
+console.log(7, { promiseArray });
+
+Promise.all(promiseArray).then((data) => {
+  console.log("All the promises are done");
+  console.log(8, { data });
+});
+
+Promise.race(promiseArray).then((data) => {
+  console.log("One of the promises is done. I don't care which!");
+  console.log(9, { data });
+});

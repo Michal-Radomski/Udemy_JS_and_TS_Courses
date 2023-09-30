cdebc9e9215740a10e8787ef33d105f9c22237b3
Michal Radomski <michalra@op.pl>
2023-09-30 14:42:08 +0200

    Course - commit 43

--- 05 ES6, ES7, ES8, EcmaScript/03/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/03/index.ts
@@ -96,6 +96,7 @@
 // });
 
 // import { apiKey } from "./keys";
+
 // import { JSDOM } from "jsdom";
 // const { window } = new JSDOM("");
 // const $ = require("jquery")(window);
@@ -325,53 +326,118 @@
 // two();
 // console.log("Last line of the code");
 
-import mysql from "mysql";
+// import mysql, { MysqlError } from "mysql";
+// // const mysql = require("mysql");
+// // import { MysqlError } from "mysql";
 
-import { email, mySQLconfig, name } from "./keys";
-const connection = mysql.createConnection(mySQLconfig);
-// console.log({ connection });
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-const checkLoginQuery = `SELECT * from users WHERE name = "${name}" AND email = "${email}"`;
-
-// connection.query(checkLoginQuery, (error, results) => {
-//   if (error) {
-//     console.error({ error });
-//     throw error;
+// import { email, mySQLconfig, name } from "./keys";
+// const connection = mysql.createConnection(mySQLconfig);
+// // console.log({ connection });
+// connection.connect(function (err: MysqlError) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
 //   }
-//   if (results.length === 1) {
-//     console.log({ results });
-//   }
+//   console.log("connected as id " + connection.threadId);
 // });
 
-function checkLogin() {
+// const checkLoginQuery = `SELECT * from users WHERE name = "${name}" AND email = "${email}"`;
+
+// // connection.query(checkLoginQuery, (error, results) => {
+// //   if (error) {
+// //     console.error({ error });
+// //     throw error;
+// //   }
+// //   if (results.length === 1) {
+// //     console.log({ results });
+// //   }
+// // });
+
+// function checkLogin() {
+//   return new Promise((resolve, reject) => {
+//     connection.query(checkLoginQuery, (error: MysqlError, results: any[]) => {
+//       if (error) {
+//         console.error({ error });
+//         reject(error);
+//       } else if (results.length == 1) {
+//         console.log({ results });
+//         resolve(results[0].id);
+//       } else {
+//         resolve(false);
+//       }
+//     });
+//   });
+// }
+
+// (async function displayData() {
+//   console.log("Displaying data...");
+//   const userId = await checkLogin();
+//   console.log(1, { userId });
+//   if (userId) {
+//     console.log(2, { userId });
+//     console.log("User Displayed");
+//   }
+// })();
+
+import { apiKey } from "./keys";
+
+import { JSDOM } from "jsdom";
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
+
+const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const peopleUrl = `https://api.themoviedb.org/3/person`;
+const castUrl = `https://api.themoviedb.org/3/movie`;
+
+function getMovieData(movieTitle: string): Promise<Object[]> {
   return new Promise((resolve, reject) => {
-    connection.query(checkLoginQuery, (error, results) => {
-      if (error) {
-        console.error({ error });
-        reject(error);
-      } else if (results.length == 1) {
-        console.log({ results });
-        resolve(results[0].id);
-      } else {
-        resolve(false);
-      }
+    $.ajax({
+      url: apiUrl + movieTitle,
+      method: "get",
+      success: (movieData: any) => {
+        // console.log(movieData)
+        resolve(movieData.results);
+      },
+      error: (errorMsg: Error) => {
+        reject(errorMsg);
+      },
     });
   });
 }
 
-(async function displayData() {
-  console.log("Displaying data...");
-  const userId = await checkLogin();
-  console.log(1, { userId });
-  if (userId) {
-    console.log(2, { userId });
-    console.log("User Displayed");
-  }
-})();
+function getCast(movie: { id: string }): Promise<Object[]> {
+  // console.log(movie)
+  return new Promise((resolve, _reject) => {
+    $.ajax({
+      url: `${castUrl}/${movie.id}/credits?api_key=${apiKey}`,
+      method: "get",
+      success: (castData: any) => {
+        resolve(castData.cast);
+      },
+    });
+  });
+}
+
+function getPerson(person: { id: string }): Promise<Object[]> {
+  return new Promise((resolve, _reject) => {
+    $.ajax({
+      url: `${peopleUrl}/${person.id}?api_key=${apiKey}`,
+      success: (personData: any) => {
+        resolve(personData);
+      },
+    });
+  });
+}
+
+async function displayMovie(movieTitle: string, index: number) {
+  const movieData = await getMovieData(movieTitle);
+  const castData = await getCast((movieData as any[])[0]);
+  const personData = await getPerson((castData as any[])[0]);
+  console.log(index, { personData });
+}
+
+const set = new Set(["die hard", "gladiator"]);
+const movieElems = Array.from(set);
+movieElems.forEach((movie, index) => {
+  displayMovie(movie, index);
+});

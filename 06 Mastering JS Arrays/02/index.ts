a115e1ebb6ffbb7665ee266e0d6d5f47f797872b
Michal Radomski <michalra@op.pl>
2023-10-07 00:44:42 +0200

    Course - commit 09

--- 06 Mastering JS Arrays/02/index.ts
+++ 06 Mastering JS Arrays/02/index.ts
@@ -82,14 +82,92 @@
 //   Array.from([1, 2, 3], (x) => x + x)
 // ); // Expected output: Array [2, 4, 6]
 
-const isArrayLike = (coll: any) => coll !== null && typeof coll[Symbol.iterator] === "function";
+// const isArrayLike = (coll: any) => coll !== null && typeof coll[Symbol.iterator] === "function";
 
-let obj = {
-  1: "one",
-  2: "two",
-  3: "three",
-};
-const str = "string";
+// let obj = {
+//   1: "one",
+//   2: "two",
+//   3: "three",
+// };
+// const str = "string";
 
-console.log("isArrayLike(obj):", isArrayLike(obj));
-console.log("isArrayLike(str):", isArrayLike(str));
+// console.log("isArrayLike(obj):", isArrayLike(obj));
+// console.log("isArrayLike(str):", isArrayLike(str));
+
+// console.log("typeof str[Symbol.iterator]:", typeof str[Symbol.iterator]);
+
+//* Methods that Mutate Array
+// const arr = ["Steven", "Mary", "Simone", "Ari", "McKay", "James"];
+// arr.reverse();
+// console.log({ arr });
+
+// let arr1 = ["Steven", "Mary", "Simone", "ari", "McKay", "James"];
+// let arr2 = [15, -10, 500, 43, -25, 0, 323, 112];
+// // arr1.sort() //* alphabetical order!;
+// // arr2.sort() //* alphabetical order!;
+
+// const compareNum = function (a: number, b: number) {
+//   // if (a < b) {
+//   //   return -1;
+//   // } else if (b < a) {
+//   //   return 1;
+//   // } else {
+//   //   return 0;
+//   // }
+//   // return (a < b) ? 1 : (b < a) ? -1 : 0;
+//   return a - b;
+// };
+
+// const compareString = function (a: string, b: string) {
+//   const x = a.toLowerCase(),
+//     y = b.toLowerCase();
+
+//   if (x < y) return -1;
+//   if (y < x) return 1;
+//   return 0;
+// };
+
+// arr1.sort(function (a, b) {
+//   let x = a.toLowerCase(),
+//     y = b.toLowerCase();
+//   if (x < y) return -1;
+//   if (y < x) return 1;
+//   return 0;
+// });
+// arr2.sort(compareNum);
+// console.log({ arr1, arr2 });
+
+const arr = [
+  {
+    firstName: "Steven",
+    lastName: "Hancock",
+    score: 90,
+  },
+  {
+    firstName: "Lynette",
+    lastName: "Jorgensen",
+    score: 100,
+  },
+  {
+    firstName: "Andrew",
+    lastName: "Wilson",
+    score: 71,
+  },
+  {
+    firstName: "Annika",
+    lastName: "Turner",
+    score: 82,
+  },
+];
+
+arr.sort(function (a, b) {
+  if (a.firstName < b.firstName) return -1;
+  if (b.firstName < a.firstName) return 1;
+  return 0;
+});
+console.log({ arr });
+
+arr.sort(function (a, b) {
+  return a.score - b.score;
+});
+console.log({ arr });

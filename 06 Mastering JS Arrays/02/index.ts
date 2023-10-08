af959391132ad83a3dd49be4402e05d722cd12e5
Michal Radomski <michalra@op.pl>
2023-10-08 17:22:01 +0200

    Course - commit 21

--- 06 Mastering JS Arrays/02/index.ts
+++ 06 Mastering JS Arrays/02/index.ts
@@ -526,25 +526,54 @@
 // console.log({ both });
 
 //* findIndex()
-const array1 = [5, 12, 8, 130, 44];
-const isLargeNumber = (element: number) => element > 13;
-console.log("array1.findIndex(isLargeNumber):", array1.findIndex(isLargeNumber)); // Expected output: 3
+// const array1 = [5, 12, 8, 130, 44];
+// const isLargeNumber = (element: number) => element > 13;
+// console.log("array1.findIndex(isLargeNumber):", array1.findIndex(isLargeNumber)); // Expected output: 3
 
-const scores = ["80", 90, "55", 60, 100, 85, 95, 25, "100"];
-const topScore = scores.findIndex(function (val) {
-  return val === 100;
-});
-console.log({ topScore });
+// const scores = ["80", 90, "55", 60, 100, 85, 95, 25, "100"];
+// const topScore = scores.findIndex(function (val) {
+//   return val === 100;
+// });
+// console.log({ topScore });
 
-const users = [
-  { name: "Steve", q1: 45, q2: 85 },
-  { name: "Ignacio", q1: 100, q2: 100 },
-  { name: "Alvin", q1: 95, q2: 100 },
-  { name: "Brianna", q1: 100, q2: 85 },
-  { name: "Andrea", q1: 75, q2: 95 },
-];
+// const users = [
+//   { name: "Steve", q1: 45, q2: 85 },
+//   { name: "Ignacio", q1: 100, q2: 100 },
+//   { name: "Alvin", q1: 95, q2: 100 },
+//   { name: "Brianna", q1: 100, q2: 85 },
+//   { name: "Andrea", q1: 75, q2: 95 },
+// ];
 
-const both = users.findIndex((val) => val.q1 === 100 && val.q2 === 100);
-console.log({ both });
+// const both = users.findIndex((val) => val.q1 === 100 && val.q2 === 100);
+// console.log({ both });
 
 //* Exercises
+// const objs = [
+//   { id: "LI_34", name: "Steven", q1: 85, q2: 0 },
+//   { id: "WL_100", name: "Mary", q1: 95, q2: 100 },
+//   { id: "LI_25", name: "Lynette", q1: 45, q2: 50 },
+//   { id: "LI_290", name: "Emily", q1: 65, q2: 30 },
+//   { id: "WL_97", name: "Sarah", q1: 35, q2: 80 },
+// ];
+
+// const getUser = function (id: string) {
+//   id = id.toUpperCase();
+//   if (objs.some((val) => val.id.toUpperCase() === id)) {
+//     return objs.find((elem) => elem.id.toUpperCase() === id);
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(getUser("LI_290"));
+
+// const stateInfo = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0];
+// const getPagePosition = function () {
+//   if (stateInfo.includes(0)) {
+//     return stateInfo.indexOf(0);
+//   } else {
+//     return stateInfo.length - 1;
+//   }
+// };
+// console.log(getPagePosition());
+
+//* Maps and Sets

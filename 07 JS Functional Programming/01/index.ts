de8b18b4735815aa5070774e12810930567d2575
Michal Radomski <michalra@op.pl>
2023-10-09 00:24:17 +0200

    Course - commit 02

--- 07 JS Functional Programming/01/index.ts
+++ 07 JS Functional Programming/01/index.ts
@@ -42,25 +42,100 @@
 // console.log("MAINAPP.returnUsers():", MAINAPP.returnUsers());
 
 //! Side-effect
-let cnt = 0;
-const increment = function () {
-  cnt++;
-  return cnt;
-};
+// let cnt = 0;
+// const increment = function () {
+//   cnt++;
+//   return cnt;
+// };
 
 //* Avoiding side-effect -> Pure Function
-const increment2 = function (num: number) {
-  return num + 1;
-};
+// const increment2 = function (num: number) {
+//   return num + 1;
+// };
 
 //* Avoiding side-effect -> Pure Function
-const average = function (scores: number[]) {
-  let total = 0;
-  for (let i = 0; i < scores.length; i++) {
-    total += scores[i];
-  }
-  return total / scores.length;
-};
-console.log("average([90, 30, 40, 50, 60]):", average([90, 30, 40, 50, 60]));
+// const average = function (scores: number[]) {
+//   let total = 0;
+//   for (let i = 0; i < scores.length; i++) {
+//     total += scores[i];
+//   }
+//   return total / scores.length;
+// };
+// console.log("average([90, 30, 40, 50, 60]):", average([90, 30, 40, 50, 60]));
 
 //* Exercise
+//! Side Effect
+// var currentUser = 0,
+//     users = [{name: "James",score: 30,tries: 1}, {name: "Mary", score: 110,tries: 4}, {name: "Henry",score: 80,tries: 3}];
+
+// var updateScore = function(newAmt: number) {
+//     users[currentUser].score += newAmt;
+// };
+
+// var returnUsers = function() {
+//     return users;
+// };
+
+// var updateTries = function() {
+//     users[currentUser].tries++;
+// };
+
+// var updateUser = function(newUser: number) {
+//     currentUser = newUser;
+// };
+
+//! Pure Function
+var users = [
+  { name: "James", score: 30, tries: 1 },
+  { name: "Mary", score: 110, tries: 4 },
+  { name: "Henry", score: 80, tries: 3 },
+];
+
+// Mutable Functions
+var recordData = function (arr: any[], prop: string) {
+  users.forEach(function (val, i, a) {
+    if (val.name.toLowerCase() === arr[0].toLowerCase()) {
+      (a as any)[i][prop] = arr[1];
+    }
+  });
+};
+
+var getScore = function (arr: any[], name: string) {
+  let score;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name.toLowerCase() === name.toLowerCase()) {
+      score = arr[i].score;
+      break;
+    }
+  }
+  return [name, score];
+};
+
+var getTries = function (arr: any[], name: string) {
+  let tries;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name.toLowerCase() === name.toLowerCase()) {
+      tries = arr[i].tries;
+      break;
+    }
+  }
+  return [name, tries];
+};
+
+var updateScore = function (arr: any[], amt: number) {
+  let newAmt = arr[1] + amt;
+  return [arr[0], newAmt];
+};
+
+var updateTries = function (arr: any[]) {
+  let newTries = arr[1] + 1;
+  return [arr[0], newTries];
+};
+
+let newScore = updateScore(getScore(users, "Henry"), 30);
+recordData(newScore, "score");
+recordData(updateTries(getTries(users, "Henry")), "tries");
+
+console.log("users:", users);

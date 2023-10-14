4e1fdc8b587a9a466ec7fd6c9807a8055c744f6b
Michal Radomski <michalra@op.pl>
2023-10-14 02:49:31 +0200

    Course - commit 19

--- 07 JS Functional Programming/03/index.ts
+++ 07 JS Functional Programming/03/index.ts
@@ -1,3 +1,26 @@
-import lodash from "lodash";
+//* Lodash - doesn't work!
+// const _ = require("lodash");
+// const fp = require("lodash/fp");
 
-console.log("lodash:", lodash);
+// // console.log("lodash:", _);
+
+// // const sum = _.add(5, 2);
+// // const sum2 = fp.add(5);
+// // const sum3 = _.add(5, 1);
+
+// // console.log({ sum, sum3 });
+// // console.log("sum2(2):", sum2(2));
+
+// const addOne = _.map((num: number) => num + 1);
+// const multiByThree = _.map((num: number) => num * 3);
+
+// const removeNumsOver100 = _.filter((num: number) => num <= 100);
+// const logAndReturn = function (data: number[]) {
+//   // console.log({ data });
+//   return data;
+// };
+
+// const processNumbers = _.flow(addOne, multiByThree, removeNumsOver100, logAndReturn);
+// console.log("processNumbers([5, 8, 20, 40]):", processNumbers([5, 8, 20, 40]));
+
+//* Exercise

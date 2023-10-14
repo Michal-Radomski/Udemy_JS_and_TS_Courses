f0bba6fc98d59cd3281b3b8d7b8d4059b1918fc8
Michal Radomski <michalra@op.pl>
2023-10-15 00:03:34 +0200

    Course - commit 21 - course done

--- 07 JS Functional Programming/03/index.ts
+++ 07 JS Functional Programming/03/index.ts
@@ -35,20 +35,57 @@
 // console.log("processScores(scores):", processScores(scores));
 
 //* Ramda
-const R = require("ramda");
-// console.log({ R });
+// const R = require("ramda");
+// // console.log({ R });
 
-const scores = [50, 6, 100, 0, 10, 75, 8, 60, 90, 80, 0, 30, 110];
+// const scores = [50, 6, 100, 0, 10, 75, 8, 60, 90, 80, 0, 30, 110];
 
-const addOne = R.map((num: number) => num + 1);
-const multiByThree = R.map((num: number) => num * 3);
-const removeNumsOver100 = R.filter((num: number) => num <= 100);
-const logAndReturn = function (data: number[]) {
-  // console.log({ data });
-  return data;
-};
-const sumAllNumbers = R.reduce((sum: number, num: number) => sum + num)(0);
+// const addOne = R.map((num: number) => num + 1);
+// const multiByThree = R.map((num: number) => num * 3);
+// const removeNumsOver100 = R.filter((num: number) => num <= 100);
+// const logAndReturn = function (data: number[]) {
+//   // console.log({ data });
+//   return data;
+// };
+// const sumAllNumbers = R.reduce((sum: number, num: number) => sum + num)(0);
 
-const processNumbers = R.pipe(addOne, multiByThree, removeNumsOver100, logAndReturn, sumAllNumbers, console.log);
+// const processNumbers = R.pipe(addOne, multiByThree, removeNumsOver100, logAndReturn, sumAllNumbers, console.log);
 
-processNumbers(scores);
+// processNumbers(scores);
+
+//* Exercise
+// interface User {
+//   name: string;
+//   score: number;
+//   tries: number;
+// }
+
+// const users = [
+//   { name: "James", score: 30, tries: 1 },
+//   { name: "Mary", score: 110, tries: 4 },
+//   { name: "Henry", score: 80, tries: 3 },
+// ];
+
+// const storeUser = (arr: User[], user: User) =>
+//   R.map((val: { name: string }) => (R.eqBy(R.toLower, val.name, user.name) ? user : val), arr);
+
+// const getUser = function (arr: User[], name: string) {
+//   return R.reduce((obj: User, val: User) => (R.eqBy(R.toLower, val.name, name) ? R.clone(val) : obj), null, arr);
+// };
+
+// const updateScore = (user: User, newAmt: number) => R.assoc("score", user.score + newAmt, user);
+
+// const updateTries = (user: User) => R.assoc("tries", user.tries + 1, user);
+
+// const getUsersUser = R.curry(getUser)(users);
+
+// const getHenry = () => getUsersUser("Henry");
+
+// const updateHenry = R.pipe(R.curry(updateScore)(getHenry()), updateTries, R.curry(storeUser)(users), console.log);
+// updateHenry(120);
+
+//* Recursion
+const factorial: Function = (num: number) => (num === 1 ? 1 : num * factorial(num - 1));
+
+const factorial_5 = factorial(5);
+console.log({ factorial_5 });

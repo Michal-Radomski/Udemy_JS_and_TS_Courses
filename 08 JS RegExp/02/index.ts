09d6c01ed7e1863fc7ea72155c4c5a26403e5497
Michal Radomski <michalra@op.pl>
2023-10-22 03:03:41 +0200

    Course - commit 23

--- 08 JS RegExp/02/index.ts
+++ 08 JS RegExp/02/index.ts
@@ -46,3 +46,31 @@ const newNames = names.map(function (name: string) {
   return name.replace(/(\w+), (\w+)/, "$2 $1");
 });
 console.log("newNames:", newNames); //* [ 'James Smith', 'Alyssa Peterson', 'Lynette Johnson', 'Tony Lopez' ]
+
+//* Matching a Word Next to Another Word
+// const str2 = "Lets put several different words more text together and see what we can match.";
+// const re4 = /\b(?:words\W+(?:\w+\W+){0,5}together)/g;
+// console.log("re4.test(str2):", re4.test(str2));
+
+//* Validating Dates: dd/mm/yyyy or d/m/yy
+const date = "24/06/1966";
+const dateRe = /^(3[01]|[12][0-9]|0?[1-9])\/(1[0-2]|0?[1-9])\/([0-9]{2})?[0-9]{2}$/g;
+console.log("dateRe.test(date):", dateRe.test(date));
+
+const dateRe2 = /^(3[01]|[12][0-9]|0?[1-9])\/(1[0-2]|0?[1-9])\/20[0-9]{2}$/g;
+const date2 = "24/06/2006";
+console.log("dateRe2.test(date2):", dateRe2.test(date2));
+
+//* Validating IP Numbers
+const IP_num1 = "25.013.55.255";
+const IP_num2 = "192.32.1.1";
+const IP_num3 = "832.26.123.5";
+const IP_num4 = "0.1.12.1";
+const IP_num5 = "255.255.255.1";
+
+// const regExpIP = /^(([0-9]|[1-9][0-9]|1[1-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[1-9]{2}|2[0-4][0-9]|25[0-5])$/;
+const regExpIP =
+  /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+const arr_IP = [IP_num1, IP_num2, IP_num3, IP_num4, IP_num5];
+const testIP = arr_IP.map((IP) => regExpIP.test(IP));
+console.log(testIP);

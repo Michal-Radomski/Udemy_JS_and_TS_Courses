94bfcf12358d0adb462834c21671f818c6c5103f
Michal Radomski <michalra@op.pl>
2023-10-30 00:43:40 +0100

    Course - commit 37

--- 09 JS Advanced Topics/02/index.ts
+++ 09 JS Advanced Topics/02/index.ts
@@ -19,40 +19,62 @@
 
 // console.log(yellName("Steven Hancock"));
 
-//* OOP in JS
+//* OOP in JS (V5 - class -> later!!!)
+// interface Obj {
+//   greeting?: string;
+//   greet?: Function;
+// }
+
+// //* V1 - Object
+// const objProto = {
+//   // greet: function () {
+//   //   console.log(this.greeting + " World");
+//   // },
+//   greet() {
+//     console.log(this.greeting + " World");
+//   },
+// } as Obj;
+
+// //* V2 - constructor function
+// const Greeting = function (this: any, term: string) {
+//   this.greeting = term;
+// };
+// Greeting.prototype = objProto;
+// const obj1a = new (Greeting as any)("Hi");
+// const obj1b = new (Greeting as any)("hi");
+
+// //* V3 - Object.create()
+// const obj2 = Object.create(objProto);
+// obj2.greeting = "Hello";
+
+// //* V4- Object.setPrototypeOf()
+// const obj3 = {
+//   greeting: "Hi",
+// } as Obj;
+// Object.setPrototypeOf(obj3, objProto);
+
+// obj1a.greet();
+// obj1b.greet();
+// obj2.greet();
+// obj3.greet?.();
+
+//* Constructor property
 interface Obj {
   greeting?: string;
   greet?: Function;
 }
-
 const objProto = {
-  // greet: function () {
-  //   console.log(this.greeting + " World");
-  // },
-  greet() {
+  greet: function () {
     console.log(this.greeting + " World");
   },
 } as Obj;
 
-// V1
 const Greeting = function (this: any, term: string) {
   this.greeting = term;
 };
+
 Greeting.prototype = objProto;
+Greeting.prototype.constructor = Greeting;
+
 const obj1a = new (Greeting as any)("Hi");
-const obj1b = new (Greeting as any)("hi");
-
-// V2
-const obj2 = Object.create(objProto);
-obj2.greeting = "Hello";
-
-// V3
-const obj3 = {
-  greeting: "Hi",
-} as Obj;
-Object.setPrototypeOf(obj3, objProto);
-
 obj1a.greet();
-obj1b.greet();
-obj2.greet();
-obj3.greet?.();

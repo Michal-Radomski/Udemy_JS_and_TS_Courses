f29dc687e851ba70b35fcd9a5e2e77e54a9ffb40
Michal Radomski <michalra@op.pl>
2023-11-05 00:09:51 +0100

    Course - commit 03

--- 10 JS Critical Parts/01/index.ts
+++ 10 JS Critical Parts/01/index.ts
@@ -1,13 +1,54 @@
 //@ Critical JS Concepts
 // console.log("globalThis:", globalThis);
 
-function recursion(num: number) {
-  num = num + num;
-  if (num > 99999999999) {
-    console.log(num);
-    return;
-  }
-  recursion(num);
-}
+//* Memory Heap and Call Stack
+// function recursion(num: number) {
+//   num = num + num;
+//   console.log({ num });
+//   if (num > 999999999999) {
+//     console.log("num:", num);
+//     return;
+//   }
+//   recursion(num);
+// }
 
-recursion(1);
+// recursion(1);
+
+//* Garbage Collection + Removing Event Listeners
+// let cnt = 0;
+// const title = document.getElementById("title") as HTMLElement;
+
+// function titleClick() {
+//   cnt++;
+//   console.log("The number of times title has been clicked: " + cnt);
+//   if (cnt > 5) {
+//     title.removeEventListener("click", titleClick);
+//   }
+// }
+
+// title.addEventListener("click", titleClick);
+
+//* Exercise
+// const arr = Array(123000).fill(1);
+// // console.log({ arr });
+
+// const popIt = function () {
+//   arr.pop();
+//   if (arr.length > 0) {
+//     // popIt();
+//     setTimeout(popIt, 0);
+//   }
+// };
+
+// popIt();
+
+//@ Critical Fundamentals
+//* Execution Context
+const num = 10;
+console.log({ num });
+
+numFunction();
+function numFunction() {
+  console.log("in numFunction");
+}
+console.log({ num });

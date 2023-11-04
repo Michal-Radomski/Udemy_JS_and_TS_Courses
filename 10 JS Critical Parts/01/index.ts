2f97ab0da51a7e6e5669070190bc82636d8ff103
Michal Radomski <michalra@op.pl>
2023-11-04 14:37:48 +0100

    Course - commit 02

--- 10 JS Critical Parts/01/index.ts
+++ 10 JS Critical Parts/01/index.ts
@@ -1,2 +1,13 @@
 //@ Critical JS Concepts
-console.log("globalThis:", globalThis);
+// console.log("globalThis:", globalThis);
+
+function recursion(num: number) {
+  num = num + num;
+  if (num > 99999999999) {
+    console.log(num);
+    return;
+  }
+  recursion(num);
+}
+
+recursion(1);

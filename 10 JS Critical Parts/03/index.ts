aadd9e3bba9817ebd302644cb888fa449f9aa276
Michal Radomski <michalra@op.pl>
2023-11-18 01:17:30 +0100

    Course - commit 65

---
+++ 10 JS Critical Parts/03/index.ts
@@ -0,0 +1,53 @@
+//@ JS Modules
+//* Module Pattern: Basic form (using literal notation)
+// const MyApp = {} as any;
+// MyApp.MyModule = (function () {
+//   const privateVariable = 1;
+//   const publicVariable = 2;
+//   const publicFunction = function () {
+//     return 3;
+//   };
+//   return {
+//     publicVariable: publicVariable,
+//     publicFunction: publicFunction,
+//   };
+// })();
+// console.log(MyApp.MyModule.privateVariable); // undefined
+// console.log(MyApp.MyModule.publicVariable); // 2
+// console.log(MyApp.MyModule.publicFunction()); // 3
+
+//* Module Pattern: Basic form (using an object)
+// const MyApp = {} as any;
+// MyApp.MyModule = (function () {
+//   const module = {} as any;
+//   const privateVariable = 1;
+//   module.publicVariable = 2;
+//   module.publicFunction = function () {
+//     return 3;
+//   };
+//   return module;
+// })();
+// console.log(MyApp.MyModule.privateVariable); // undefined
+// console.log(MyApp.MyModule.publicVariable); // 2
+// console.log(MyApp.MyModule.publicFunction()); // 3
+
+//* Revealing Module Pattern
+const createSupplier = (function () {
+  // Private
+  const name = "General Motors";
+  const field = "automobile";
+  const getSupplierName = () => name;
+  const getSupplierField = () => field;
+
+  // Public
+  return {
+    name,
+    field,
+    getName: () => getSupplierName(),
+    getField: () => getSupplierField(),
+  };
+})();
+
+console.log("createSupplier.name:", createSupplier.name);
+console.log("createSupplier.field:", createSupplier.field);
+console.log("createSupplier.getName():", createSupplier.getName());

c0c91fe567a6595a16649a13e27973d056ecc2cb
Michal Radomski <michalra@op.pl>
2023-11-18 13:16:19 +0100

    Course - commit 67

---
+++ 10 JS Critical Parts/03/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const utils = require("./utils");
+
+const name = "Hancock, Steven";
+
+console.log("This is the main file.");
+console.log("name:", utils.upperCaseName(utils.formatName(name)));

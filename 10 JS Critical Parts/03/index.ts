9f36a02744d2490d0136cc96350054d19b6bb5de
Michal Radomski <michalra@op.pl>
2023-11-19 22:57:13 +0100

    Course - commit 76

--- 10 JS Critical Parts/03/index.ts
+++ 10 JS Critical Parts/03/index.ts
@@ -123,144 +123,191 @@
 // console.log("array[1]:", array[1]);
 
 //* Linked List Example + Exercise
-class LinkedNode {
-  value: number;
-  next: null;
-  constructor(val: number) {
-    this.value = val;
-    this.next = null;
-  }
-}
+// class LinkedNode {
+//   value: number;
+//   next: null;
+//   constructor(val: number) {
+//     this.value = val;
+//     this.next = null;
+//   }
+// }
 
-class LinkedList {
-  head: LinkedNode | null;
-  tail: LinkedNode | null;
+// class LinkedList {
+//   head: LinkedNode | null;
+//   tail: LinkedNode | null;
+//   size: number;
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.size = 0;
+//   }
+
+//   // Add node to list
+//   add(val: number) {
+//     // console.log({ val });
+//     const node = new LinkedNode(val);
+//     if (!this.head) {
+//       this.head = node;
+//       this.tail = node;
+//     } else {
+//       this!.tail!.next = node as any;
+//       this.tail = node;
+//     }
+//     this.size++;
+//     return this;
+//   }
+
+//   // Remove the tail from list
+//   remove() {
+//     if (!this.head) return undefined;
+
+//     // find the node right before the tail
+//     let curNode = this.head;
+//     let newTail = curNode;
+//     while (curNode.next) {
+//       newTail = curNode;
+//       curNode = curNode.next;
+//     }
+//     // set the new tail
+//     newTail.next = null;
+//     this.tail = newTail;
+//     // establish size
+//     this.size--;
+//     if (this.size === 0) {
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return curNode;
+//   }
+
+//   // Insert a node at a specific index
+//   insertAt(val: number, index: number) {
+//     // console.log({ val, index });
+//     if (index < 0 || index > this.size) return null;
+//     if (index === this.size) return this.add(val);
+//     const node = new LinkedNode(val);
+
+//     if (index === 0) {
+//       node.next = this.head as any;
+//       this.head = node;
+//     } else {
+//       let prevNode: any = this.getNode(index - 1);
+//       let tmpNode = prevNode.next;
+//       node.next = tmpNode;
+//       prevNode.next = node;
+//     }
+//     this.size++;
+//     return node;
+//   }
+
+//   // Remove node from specific index
+
+//   removeAt(index: number) {
+//     // console.log({ index });
+//     if (index < 0 || index > this.size) return null;
+//     if (index === this.size - 1) return this.remove();
+
+//     let currentNode, nextNode, prevNode;
+//     if (index === 0) {
+//       currentNode = this.head;
+//       nextNode = currentNode?.next;
+//       currentNode!.next = null;
+//       this.head = nextNode as any;
+//     } else {
+//       prevNode = this.getNode(index - 1);
+//       currentNode = (prevNode as any).next;
+//       (prevNode as any).next = currentNode.next;
+//       currentNode.next = null;
+//     }
+//     this.size--;
+//     return currentNode;
+//   }
+
+//   // Get index for specific node
+//   // Not currently coded to work for objects; only primitives
+//   getIndex(val: number) {
+//     // console.log({val})
+//     let currentNode = this.head;
+//     let cnt = 0;
+
+//     while (currentNode !== null) {
+//       if (currentNode.value === val) {
+//         return cnt;
+//       }
+//       cnt++;
+//       currentNode = currentNode.next;
+//     }
+//     return -1;
+//   }
+
+//   // Get node for specific index
+//   getNode(index: number) {
+//     console.log({ index });
+//     if (index < 0 || index >= this.size) return null;
+//     if (index === this.size - 1) return this.tail;
+
+//     let cnt = 0,
+//       currentNode = this.head;
+//     // find node at index entered
+//     while (cnt !== index) {
+//       currentNode = currentNode!.next;
+//       cnt++;
+//     }
+//     return currentNode;
+//   }
+// }
+
+// const list = new LinkedList();
+// list.add(56);
+// list.add(76);
+// list.add(100);
+// list.add(101);
+// list.remove();
+// console.log("list:", list, typeof list);
+
+//* Stack Intro
+// (function testFunction() {
+//   console.log("Test function");
+//   function one(num: number) {
+//     console.log(num, "Function one");
+//     two(2);
+//   }
+//   function two(num: number) {
+//     console.log(num, "Function two");
+//   }
+//   one(1);
+//   two(3);
+// })();
+
+//* Stack Data Structure
+class Stack {
+  items: number[];
   size: number;
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.items = [];
     this.size = 0;
   }
 
-  // Add node to list
-  add(val: number) {
-    // console.log({ val });
-    const node = new LinkedNode(val);
-    if (!this.head) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this!.tail!.next = node as any;
-      this.tail = node;
-    }
-    this.size++;
-    return this;
+  push(item: number) {
+    this.items.push(item);
+    return ++this.size;
   }
-
-  // Remove the tail from list
-  remove() {
-    if (!this.head) return undefined;
-
-    // find the node right before the tail
-    let curNode = this.head;
-    let newTail = curNode;
-    while (curNode.next) {
-      newTail = curNode;
-      curNode = curNode.next;
-    }
-    // set the new tail
-    newTail.next = null;
-    this.tail = newTail;
-    // establish size
+  pop() {
+    if (this.size === 0) return null;
     this.size--;
-    if (this.size === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return curNode;
+    return this.items.pop();
   }
 
-  // Insert a node at a specific index
-  insertAt(val: number, index: number) {
-    // console.log({ val, index });
-    if (index < 0 || index > this.size) return null;
-    if (index === this.size) return this.add(val);
-    const node = new LinkedNode(val);
-
-    if (index === 0) {
-      node.next = this.head as any;
-      this.head = node;
-    } else {
-      let prevNode: any = this.getNode(index - 1);
-      let tmpNode = prevNode.next;
-      node.next = tmpNode;
-      prevNode.next = node;
-    }
-    this.size++;
-    return node;
-  }
-
-  // Remove node from specific index
-
-  removeAt(index: number) {
-    // console.log({ index });
-    if (index < 0 || index > this.size) return null;
-    if (index === this.size - 1) return this.remove();
-
-    let currentNode, nextNode, prevNode;
-    if (index === 0) {
-      currentNode = this.head;
-      nextNode = currentNode?.next;
-      currentNode!.next = null;
-      this.head = nextNode as any;
-    } else {
-      prevNode = this.getNode(index - 1);
-      currentNode = (prevNode as any).next;
-      (prevNode as any).next = currentNode.next;
-      currentNode.next = null;
-    }
-    this.size--;
-    return currentNode;
-  }
-
-  // Get index for specific node
-  // Not currently coded to work for objects; only primitives
-  getIndex(val: number) {
-    // console.log({val})
-    let currentNode = this.head;
-    let cnt = 0;
-
-    while (currentNode !== null) {
-      if (currentNode.value === val) {
-        return cnt;
-      }
-      cnt++;
-      currentNode = currentNode.next;
-    }
-    return -1;
-  }
-
-  // Get node for specific index
-  getNode(index: number) {
-    console.log({ index });
-    if (index < 0 || index >= this.size) return null;
-    if (index === this.size - 1) return this.tail;
-
-    let cnt = 0,
-      currentNode = this.head;
-    // find node at index entered
-    while (cnt !== index) {
-      currentNode = currentNode!.next;
-      cnt++;
-    }
-    return currentNode;
+  check() {
+    return this.items[this.size - 1];
   }
 }
 
-const list = new LinkedList();
-list.add(56);
-list.add(76);
-list.add(100);
-list.add(101);
-list.remove();
-console.log("list:", list, typeof list);
+const stack = new Stack();
+console.log(1, "stack:", stack);
+stack.push(5);
+stack.push(6);
+stack.push(7);
+console.log(2, "stack:", stack);
+stack.pop();
+console.log(3, "stack:", stack);

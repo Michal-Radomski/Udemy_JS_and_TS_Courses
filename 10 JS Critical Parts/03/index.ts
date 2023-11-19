21fe0b8b71a794013820ccc22a381c31a9c30882
Michal Radomski <michalra@op.pl>
2023-11-19 20:07:54 +0100

    Course - commit 73

--- 10 JS Critical Parts/03/index.ts
+++ 10 JS Critical Parts/03/index.ts
@@ -63,32 +63,61 @@
 // console.log("yellName('Steven Hancock'):", yellName("Steven Hancock"));
 
 //@ Data Structures
-//* The Big O Notation
-// O(n) -> Linear
-const factor = function (num: number) {
-  let result = num;
-  if (num === 0 || num === 1) return 1;
-  while (num > 1) {
-    num--;
-    result *= num;
-  }
-  return result;
-};
-console.log("factor(5):", factor(5));
+// //* The Big O Notation
+// // O(n) -> Linear
+// const factor = function (num: number) {
+//   let result = num;
+//   if (num === 0 || num === 1) return 1;
+//   while (num > 1) {
+//     num--;
+//     result *= num;
+//   }
+//   return result;
+// };
+// console.log("factor(5):", factor(5));
 
-// O(1) -> Static
-const aBigint = function (num: number) {
-  return BigInt(num * Number.MAX_SAFE_INTEGER);
-};
-console.log("aBigint(1):", aBigint(1));
+// // O(1) -> Static
+// const aBigint = function (num: number) {
+//   console.log("num / Number.MAX_SAFE_INTEGER:", num / Number.MAX_SAFE_INTEGER);
+//   return BigInt(num * Number.MAX_SAFE_INTEGER);
+// };
+// console.log("aBigint(1):", aBigint(1));
 
-// O(n^2) -> Square
-const multiplyMatrix = function (n: number) {
-  for (let i = 0; i <= n; i++) {
-    for (let j = 0; j <= n; j++) {
-      // console.log(i + " * " + j + " = " + i * j);
-      console.log(`${i}*${j} = ${i * j}`);
-    }
-  }
+// // O(n^2) -> Square
+// const multiplyMatrix = function (n: number) {
+//   for (let i = 0; i <= n; i++) {
+//     for (let j = 0; j <= n; j++) {
+//       // console.log(i + " * " + j + " = " + i * j);
+//       console.log(`${i}*${j} = ${i * j}`);
+//     }
+//   }
+// };
+// multiplyMatrix(5);
+
+//* Looking at Objects and Arrays Through Big O -> https://www.bigocheatsheet.com/
+//* Big o for objects
+// Insertion  obj.active = true;        -> O(1)
+// Deletion   delete obj.instructor;    -> O(1)
+// Searching  for (let prop in obj {})  -> O(n)
+// Access     obj.fName;                -> O(1)
+
+const obj = {
+  fName: "Steven",
+  lName: "Hancock",
+  fullName() {
+    return `${this.fName} ${this.lName}`;
+  },
+  instructor: true,
 };
-multiplyMatrix(5);
+console.log("obj.instructor:", obj.instructor);
+
+//* Big O for Arrays
+// Insertion  array1.push(true);           -> O(1) depends
+//            array1.unshift(false);       -> O(n)
+// Deletion   array1.pop();                -> O(1) depends
+//            array1.shift();              -> O(n)
+// Searching  for (let elem of array1) {}  -> O(n)
+// Access     array1[2];                   -> O(1)
+
+const array = [1, 2, "string", {}];
+console.log("array[1]:", array[1]);

5cbe994cec48721fd69242ce93147ae5f42f35a5
Michal Radomski <michalra@op.pl>
2023-11-21 00:51:00 +0100

    Course - commit 04

--- 11 JS Weird Parts/01/index.ts
+++ 11 JS Weird Parts/01/index.ts
@@ -37,19 +37,43 @@
 //   console.log(2, "a");
 // }
 // a();
+
 //* Variable Environments
-function b() {
-  var myVar;
-  console.log(1, { myVar });
+// function b() {
+//   var myVar;
+//   console.log(1, { myVar });
+// }
+
+// function a() {
+//   var myVar = 2;
+//   console.log(2, { myVar });
+//   b();
+// }
+
+// var myVar = 1;
+// console.log(3, { myVar });
+// a();
+// console.log(4, { myVar });
+
+//* The Scope Chain
+// function a() {
+//   function b() {
+//     console.log({ myVar }); // { myVar: 1 } from global object
+//   }
+//   // var myVar = 2;
+//   b();
+// }
+// var myVar = 1;
+// a();
+
+//* Async Callback
+// long running function
+function waitThreeSeconds() {
+  const ms = 3000 + new Date().getTime();
+  console.log({ ms });
+  while (Number(new Date()) < ms) {}
+  console.log("Finished function");
 }
 
-function a() {
-  var myVar = 2;
-  console.log(2, { myVar });
-  b();
-}
-
-var myVar = 1;
-console.log(3, { myVar });
-a();
-console.log(4, { myVar });
+waitThreeSeconds();
+console.log("Finished execution");

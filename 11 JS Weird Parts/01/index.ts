5da1bdea62a6ed18fce60a553a1329acbffe4408
Michal Radomski <michalra@op.pl>
2023-11-21 00:32:18 +0100

    Course - commit 03

--- 11 JS Weird Parts/01/index.ts
+++ 11 JS Weird Parts/01/index.ts
@@ -1,4 +1,5 @@
 //@ Execution Context and Lexical Environments
+//* Creation phase + execution phase
 // const str = "Hello World!";
 
 // (function (this: any) {
@@ -15,13 +16,40 @@
 // var a;
 // console.log({ a }); //* Undefined
 
-//* Undefined
-let a;
-console.log({ a }, typeof a);
-console.log(typeof a === "undefined"); // true
+//* Undefined -> use null instead of undefined
+// let a;
+// console.log({ a }, typeof a);
+// console.log(typeof a === "undefined"); // true
 
-if (a === undefined) {
-  console.log("a is undefined!");
-} else {
-  console.log("a is defined!");
+// if (a === undefined) {
+//   console.log("a is undefined!");
+// } else {
+//   console.log("a is defined!");
+// }
+
+//* Execution Stack
+// function b() {
+//   console.log("b");
+// }
+// function a() {
+//   console.log(1, "a");
+//   b();
+//   console.log(2, "a");
+// }
+// a();
+//* Variable Environments
+function b() {
+  var myVar;
+  console.log(1, { myVar });
 }
+
+function a() {
+  var myVar = 2;
+  console.log(2, { myVar });
+  b();
+}
+
+var myVar = 1;
+console.log(3, { myVar });
+a();
+console.log(4, { myVar });

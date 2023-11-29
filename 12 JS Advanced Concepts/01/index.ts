32556a0bf228e69f8ad826f59848b03ab60fc3af
Michal Radomski <michalra@op.pl>
2023-11-29 01:33:18 +0100

    Course - commit 06

--- 12 JS Advanced Concepts/01/index.ts
+++ 12 JS Advanced Concepts/01/index.ts
@@ -61,15 +61,64 @@
 // console.log(marry2("Tim", "Tine"));
 
 //* Variable Environment
-function two() {
-  var isValid; // 5. isValid is undefined in this execution context.
-}
+// function two() {
+//   var isValid; // 5. isValid is undefined in this execution context.
+// }
 
-function one() {
-  var isValid = true; // 3. this variable will be put into the new execution context. It's own variable environment
-  two(); // 4. New execution context created.
-}
+// function one() {
+//   var isValid = true; // 3. this variable will be put into the new execution context. It's own variable environment
+//   two(); // 4. New execution context created.
+// }
 
-var isValid = false; // 1. Global variable is created as undefined. Then during execution, it changes in memory to false.
-one(); // 2. New execution context is created on top of the stack.
-console.log({ isValid }); // false
+// var isValid = false; // 1. Global variable is created as undefined. Then during execution, it changes in memory to false.
+// one(); // 2. New execution context is created on top of the stack.
+// console.log({ isValid }); // false
+
+//* Scope Chain
+// var global_variable = "GeeksForGeeks";
+
+// // First function...
+// function first_function() {
+//   return global_variable;
+// }
+
+// // Second function...
+// function second_function() {
+//   return first_function();
+// }
+
+// console.log(second_function()); // GeeksForGeeks
+
+// function sayMyName() {
+//   var a = "a";
+//   return function findName() {
+//     var b = "b";
+//     return function printName() {
+//       var c = "c";
+//       // console.log(a);
+//       return "Andrei Neagoie";
+//     };
+//   };
+// }
+// console.log(sayMyName()()()); // Andrei Neagoie
+
+// function findName() {
+//   var b = "b";
+//   return printName();
+// }
+
+// function printName() {
+//   var c = "c";
+//   return "Andrei Neagoie";
+// }
+
+// function sayMyName2() {
+//   var a = "a";
+//   return findName();
+// }
+// console.log(sayMyName2()); // Andrei Neagoie
+
+//* Eval() -> don't use it
+console.log(eval("2 + 2")); // Expected output: 4
+console.log(eval("2 + 2") === eval("4")); // Expected output: true
+console.log(eval("2 + 2") === eval(new String("2 + 2") as any)); // Expected output: false

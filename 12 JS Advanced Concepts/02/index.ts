dfb7f328b8a54529c75c59cd8bb814a17b420425
Michal Radomski <michalra@op.pl>
2023-12-04 00:53:22 +0100

    Course - commit 43

--- 12 JS Advanced Concepts/02/index.ts
+++ 12 JS Advanced Concepts/02/index.ts
@@ -16,16 +16,46 @@
 // console.log({ tiger, rest });
 
 //* Job Queue: Job queue has a higher priority than callback queue!
-// Callback Queue - Task Queue
-setTimeout(() => {
-  console.log("1", "Is the loneliest number");
-}, 0);
-setTimeout(() => {
-  console.log("2", "Can be as bad as one");
-}, 10);
+//* Callback Queue - Task Queue
+// setTimeout(() => {
+//   console.log("1", "Is the loneliest number");
+// }, 0);
+// setTimeout(() => {
+//   console.log("2", "Can be as bad as one");
+// }, 10);
 
-// Job Queue - Microtask Queue
-Promise.resolve("Hi").then((data) => console.log("3", "data:", data));
+//* Job Queue - Microtask Queue
+// Promise.resolve("Hi").then((data) => console.log("3", "data:", data));
 
-// 3
-console.log("4", "Is a crowd");
+//* 3
+// console.log("4", "Is a crowd");
+
+//* Parallel, Sequence and Race
+const promisify = (item: string, delay: number) => new Promise((resolve) => setTimeout(() => resolve(item), delay));
+
+const a = () => promisify("a", 100);
+const b = () => promisify("b", 5000);
+const c = () => promisify("c", 3000);
+
+async function parallel() {
+  const promises = [a(), b(), c()];
+  const [output1, output2, output3] = await Promise.all(promises);
+  return `Parallel is done: ${output1} ${output2} ${output3}`;
+}
+
+async function race() {
+  const promises = [a(), b(), c()];
+  const output1 = await Promise.race(promises);
+  return `Race is done: ${output1}`;
+}
+
+async function sequence() {
+  const output1 = await a();
+  const output2 = await b();
+  const output3 = await c();
+  return `Sequence is done ${output1} ${output2} ${output3}`;
+}
+
+sequence().then(console.log);
+parallel().then(console.log);
+race().then(console.log);

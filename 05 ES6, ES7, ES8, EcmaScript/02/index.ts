27b10a780bc3ad0a65a0441236d6b76215c8ad40
Michal Radomski <michalra@op.pl>
2023-12-06 00:05:09 +0100

    Course - commit 51

--- 05 ES6, ES7, ES8, EcmaScript/02/index.ts
+++ 05 ES6, ES7, ES8, EcmaScript/02/index.ts
@@ -64,7 +64,10 @@ console.log({ g_2 });
 // const h_2 = new Worker("worker.js");
 // console.log({h_2})
 
-// console.log({ a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, r, s, t, u, v, w, x, y, z, a_2, b_2, c_2, d_2, e_2, f_2, g_2, h_2 });
+const i_2 = new TextEncoder();
+console.log({ i_2 });
+
+// console.log({ a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, r, s, t, u, v, w, x, y, z, a_2, b_2, c_2, d_2, e_2, f_2, g_2, h_2, i_2 });
 
 //* Example
 // const date = new Date(Date.UTC(2020, 11, 20, 3, 23, 16, 738));

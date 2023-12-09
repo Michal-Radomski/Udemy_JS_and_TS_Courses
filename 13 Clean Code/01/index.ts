9ce215d5c737cb4ce464c069a2da07e18d1be366
Michal Radomski <michalra@op.pl>
2023-12-09 02:12:54 +0100

    Course - commit 02

--- 13 Clean Code/01/index.ts
+++ 13 Clean Code/01/index.ts
@@ -1,4 +1,9 @@
+//* https://dev.to/favourmark05/writing-clean-code-best-practices-and-principles-3amh
+//* https://www.freecodecamp.org/news/how-to-write-clean-code
+//* https://devszczepaniak.pl/solid-kiss-i-dry/
+
 //@ Naming
+//* Names should be meaningful
 class User {
   save() {}
 }
@@ -11,3 +16,23 @@ user.save();
 if (isLoggedIn) {
   // ...
 }
+
+//*  Exceptions
+class Database {
+  private client: any;
+
+  get connectedClient() {
+    if (!this.client) {
+      throw new Error("Database not connected!");
+    }
+    return this.client;
+  }
+
+  connect() {
+    // Establishing connection ...
+    this.client = {};
+  }
+}
+
+const db = new Database();
+db.connectedClient.query();

9bdfb164f774cf5595ad94876bcd790a3f75ae0c
Michal Radomski <michalra@op.pl>
2025-05-11 13:11:23 +0200

    TypeScript for Professionals course - commit 09

--- 13 TypeScript/01/index.ts
+++ 13 TypeScript/01/index.ts
@@ -145,28 +145,28 @@
   console.log(add(1, 2));
 }
 
-// {
-//   //* 07 Structural Typing
-//   type Point2D = { x: number; y: number };
-//   type Point3D = { x: number; y: number; z: number };
+{
+  //* 07 Structural Typing
+  type Point2D = { x: number; y: number };
+  type Point3D = { x: number; y: number; z: number };
 
-//   let point2D: Point2D = { x: 0, y: 10 };
-//   let point3D: Point3D = { x: 0, y: 10, z: 20 };
+  let point2D: Point2D = { x: 0, y: 10 };
+  let point3D: Point3D = { x: 0, y: 10, z: 20 };
 
-//   /** Extra info ok */
-//   point2D = point3D;
-//   function takesPoint2D(point: Point2D) {
-//     /** ... */
-//   }
-//   takesPoint2D(point3D);
+  //* Extra info ok
+  point2D = point3D;
+  function takesPoint2D(point: Point2D) {
+    console.log({ point });
+  }
+  console.log("takesPoint2D(point3D):", takesPoint2D(point3D));
 
-//   /** Error: missing info */
-//   point3D = point2D;
-//   function takesPoint3D(point: Point3D) {
-//     /** ... */
-//   }
-//   takesPoint3D(point2D);
-// }
+  //* Error: missing info
+  // point3D = point2D;
+  // function takesPoint3D(point: Point3D) {
+  //   /** ... */
+  // }
+  // takesPoint3D(point2D);
+}
 
 // {
 //   //* Classes

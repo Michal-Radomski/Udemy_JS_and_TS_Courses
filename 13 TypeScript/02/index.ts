28e260f2e4069fa8b644f4a1e040723511b4fc6d
Michal Radomski <michalra@op.pl>
2025-05-11 22:47:40 +0200

    TypeScript for Professionals course - commit 25

--- 13 TypeScript/02/index.ts
+++ 13 TypeScript/02/index.ts
@@ -386,3 +386,40 @@
     contact(person);
   })();
 }
+
+{
+  //* Interfaces & Types
+  (() => {
+    // With type aliases
+    type Point2D = {
+      x: number;
+      y: number;
+    };
+
+    type Point3D = Point2D & { z: number };
+
+    const pointWithType: Point3D = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+
+    // With instances
+    interface Point2DI {
+      x: number;
+      y: number;
+    }
+
+    interface Point3DI extends Point2DI {
+      z: number;
+    }
+
+    const pointWithInterface: Point3DI = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+
+    console.log({ pointWithType, pointWithInterface });
+  })();
+}

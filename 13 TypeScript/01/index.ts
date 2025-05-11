f07bc9dc16d84ac5bd9c0a796425d982150f4b02
Michal Radomski <michalra@op.pl>
2025-05-11 12:31:13 +0200

    TypeScript for Professionals course - commit 02

--- 13 TypeScript/01/index.ts
+++ 13 TypeScript/01/index.ts
@@ -1,3 +1,26 @@
-let message: string = "Hello world";
-message += " again";
-console.log("message:", message);
+{
+  let message: string = "Hello world";
+  message += " again";
+  console.log("message:", message);
+}
+
+{
+  const regexp: RegExp = new RegExp("ab+c");
+  const array: Array<number> = [1, 2, 3];
+  const set: Set<number> = new Set([1, 2, 3]);
+  console.log("regexp, array, set:", regexp, array, set);
+
+  /** A first in first out collection */
+  class Queue<T> {
+    private data: Array<T> = [];
+    push(item: T) {
+      this.data.push(item);
+    }
+    pop(): T | undefined {
+      return this.data.shift();
+    }
+  }
+
+  const queue: Queue<number> = new Queue();
+  console.log("queue:", queue);
+}

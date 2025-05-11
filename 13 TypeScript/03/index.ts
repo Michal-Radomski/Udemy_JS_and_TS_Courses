0600e3184cf5429aa4d17d3bcc2f60226cad0d3f
Michal Radomski <michalra@op.pl>
2025-05-11 23:42:40 +0200

    TypeScript for Professionals course - commit 29

--- 13 TypeScript/03/index.ts
+++ 13 TypeScript/03/index.ts
@@ -11,18 +11,53 @@
 
   class Cat implements Animal {
     constructor(public name: string) {}
-    voice() {
+    voice(): string {
       return "meow";
     }
   }
 
   class Dog implements Animal {
     constructor(public name: string) {}
-    voice() {
+    voice(): string {
       return "woof";
     }
   }
 
   log(new Cat("Salem")); // Animal Salem: meow
   log(new Dog("Lassie")); // Animal Lassie: woof
+}
+
+{
+  //* Definite Assignment Assertion
+  (() => {
+    let dice!: number; //* Definite assignment assertion "!" (tells TypeScript that this variable will always be assigned)
+
+    function rollDice(): void {
+      dice = Math.floor(Math.random() * 6) + 1;
+    }
+
+    rollDice();
+    console.log("Current dice value:", dice); // No complains
+
+    rollDice();
+    console.log("Current dice value:", dice); // No complains
+
+    // In classes
+    class Point {
+      x!: number;
+      y!: number;
+
+      constructor() {
+        this.moveRandom(); // Removes duplication. Needs definite assignment assertion in properties
+      }
+
+      moveRandom(): void {
+        this.x = Math.random();
+        this.y = Math.random();
+      }
+    }
+
+    const point = new Point();
+    console.log("point:", point);
+  })();
 }

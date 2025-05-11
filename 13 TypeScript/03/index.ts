e844d389c4912e25f8ee4c7c399d750058c7fca5
Michal Radomski <michalra@op.pl>
2025-05-11 23:45:12 +0200

    TypeScript for Professionals course - commit 30

--- 13 TypeScript/03/index.ts
+++ 13 TypeScript/03/index.ts
@@ -29,7 +29,7 @@
 
 {
   //* Definite Assignment Assertion
-  (() => {
+  ((): void => {
     let dice!: number; //* Definite assignment assertion "!" (tells TypeScript that this variable will always be assigned)
 
     function rollDice(): void {
@@ -59,5 +59,42 @@
 
     const point = new Point();
     console.log("point:", point);
+  })();
+}
+
+{
+  //* User Defined Type Guards
+  ((): void => {
+    type Rectangle = {
+      height: number;
+      width: number;
+    };
+
+    type Square = {
+      size: number;
+    };
+
+    type Shape = Rectangle | Square;
+
+    function isRectangle(shape: Shape): shape is Rectangle {
+      return "height" in shape && "width" in shape;
+    }
+
+    function isSquare(shape: Shape): shape is Square {
+      return "size" in shape;
+    }
+
+    function area(shape: Shape): number {
+      if (isRectangle(shape)) {
+        return shape.height * shape.width;
+      }
+      if (isSquare(shape)) {
+        return shape.size * shape.size;
+      }
+      const ensure: never = shape;
+      return ensure;
+    }
+
+    console.log("area({ size: 5 }):", area({ size: 5 }));
   })();
 }

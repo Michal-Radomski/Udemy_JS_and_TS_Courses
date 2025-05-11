75e00f2f493c5a05fe6fe7dc9764b4ca7624e33e
Michal Radomski <michalra@op.pl>
2025-05-11 12:35:18 +0200

    TypeScript for Professionals course - commit 03

--- 13 TypeScript/01/index.ts
+++ 13 TypeScript/01/index.ts
@@ -1,16 +1,33 @@
 {
+  //* Intro
   let message: string = "Hello world";
   message += " again";
   console.log("message:", message);
 }
 
 {
+  //* Primitives
+  const isPresent: boolean = false;
+  const magic: number = 66.6;
+  const hello: string = "world";
+  console.log("isPresent, magic, hello:", isPresent, magic, hello);
+
+  const notDefined: undefined = undefined;
+  const notPresent: null = null;
+  console.log("notPresent, notDefined:", notPresent, notDefined);
+
+  const penta: symbol = Symbol("star");
+  const biggy: bigint = 24n;
+  console.log("penta, biggy:", penta, biggy);
+}
+
+{
+  //* Instance Types
   const regexp: RegExp = new RegExp("ab+c");
   const array: Array<number> = [1, 2, 3];
   const set: Set<number> = new Set([1, 2, 3]);
   console.log("regexp, array, set:", regexp, array, set);
 
-  /** A first in first out collection */
   class Queue<T> {
     private data: Array<T> = [];
     push(item: T) {

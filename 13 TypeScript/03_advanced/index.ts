39a2ac29108a25e08a94c19b9e9654cd836449e1
Michal Radomski <michalra@op.pl>
2025-05-13 00:24:58 +0200

    TypeScript for Professionals course - commit 38

--- 13 TypeScript/03_advanced/index.ts
+++ 13 TypeScript/03_advanced/index.ts
@@ -360,12 +360,31 @@
     type Point = readonly [number, number];
 
     function move(point: Point, x: number, y: number): Point {
-      return [point[0] + x, point[1] + y];
+      return [point[0] + x, point[1] + y]; //* Returns a new array!
     }
 
     const point: Point = [0, 0];
     const moved: Point = move(point, 10, 10);
     console.log("point:", point); // [0, 0]
     console.log("moved:", moved); // [10, 10]
+  })();
+}
+
+{
+  //* Double Assertion
+  ((): void => {
+    type Point2D = { x: number; y: number };
+    type Point3D = Point2D & { z: number };
+    type Person = { name: string; email: string };
+
+    let point2: Point2D = { x: 0, y: 0 };
+    let point3: Point3D = { x: 10, y: 10, z: 10 };
+    let person: Person = { name: "John", email: "john.doe@example.com" };
+
+    // Force assignment with Type Assertion
+    point2 = point3; // No problem cause Point3D has x and y
+    point3 = point2 as Point3D; // Single Assertion
+    // point3 = person as Point3D; // TypeScript do not trust us
+    point3 = person as unknown as Point3D; // TypeScript trust us
   })();
 }

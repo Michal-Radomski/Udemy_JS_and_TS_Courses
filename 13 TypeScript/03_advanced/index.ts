6d92fddb5c2b7892adf1395dcb67cfc9f4ad0d09
Michal Radomski <michalra@op.pl>
2025-05-13 00:40:09 +0200

    TypeScript for Professionals course - commit 41

--- 13 TypeScript/03_advanced/index.ts
+++ 13 TypeScript/03_advanced/index.ts
@@ -448,3 +448,30 @@
     // invalid.double(); // Error
   })();
 }
+
+{
+  //* Generic Constraints
+  ((): void => {
+    type NameFields = {
+      firstName: string;
+      lastName: string;
+    };
+
+    function addFullName<T extends NameFields>(obj: T): T & { fullName: string } {
+      return { ...obj, fullName: `${obj.firstName} ${obj.lastName}` };
+    }
+
+    const john = addFullName({
+      email: "john.doe@example.com",
+      firstName: "John",
+      lastName: "Doe",
+    });
+
+    console.log("john.email:", john.email);
+    console.log("john.fullName:", john.fullName);
+
+    // const jane = addFullName({
+    //   firstName: "Jane",
+    // }); // Property lastName is missing
+  })();
+}

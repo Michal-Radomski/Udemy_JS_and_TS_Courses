a49cc5bee3eaefff29b3a32b6a835a957ea1de30
Michal Radomski <michalra@op.pl>
2025-05-13 00:53:31 +0200

    TypeScript for Professionals course - commit 43

---
+++ 13 TypeScript/04_expert/index.ts
@@ -0,0 +1,34 @@
+{
+  //* Typeof type operator
+  ((): void => {
+    const center = {
+      x: 0,
+      y: 0,
+      z: 0,
+    };
+
+    //* type Point = typeof center
+
+    // Inline
+    const unit: typeof center = {
+      x: center.x + 1,
+      y: center.y + 1,
+      z: center.z + 1,
+    };
+    console.log("unit:", unit);
+
+    // Create a type from JSON
+    const response = {
+      email: "john.doe@example.com",
+      firstName: "John",
+      lastName: "Doe",
+    };
+
+    type PersonResponse = typeof response;
+
+    function processResponse(person: PersonResponse) {
+      console.log("Full name:", `${person.firstName} ${person.lastName}`);
+    }
+    processResponse(response);
+  })();
+}

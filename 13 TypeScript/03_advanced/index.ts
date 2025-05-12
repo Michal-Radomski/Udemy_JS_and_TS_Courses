5c4c2c3a9320dde5810abe6d387e705527c97856
Michal Radomski <michalra@op.pl>
2025-05-13 00:09:00 +0200

    TypeScript for Professionals course - commit 36

--- 13 TypeScript/03_advanced/index.ts
+++ 13 TypeScript/03_advanced/index.ts
@@ -246,3 +246,49 @@
     }
   })();
 }
+
+{
+  //* Index Signatures
+  ((): void => {
+    const str = {
+      hello: "world",
+    };
+
+    console.log(str["hello"]); // world
+
+    const nums = {
+      1337: "leet",
+    };
+
+    console.log(nums[1337]); // leet
+
+    // Index signature example
+    type Dictionary = {
+      [key: string]: boolean;
+    };
+
+    // Use
+    type Person = {
+      displayName: string;
+      email: string;
+    };
+
+    type PersonDictionary = {
+      [username: string]: Person | undefined; // Avoid runtime error
+    };
+
+    const persons: PersonDictionary = {
+      jane: { displayName: "Jane Doe", email: "jane.doe@example.com" },
+    };
+    persons["john"] = { displayName: "John Doe", email: "john.doe@example.com" };
+
+    console.log(persons["john"]);
+    delete persons["john"];
+
+    // const result = persons["missing"];
+    // console.log(result, result?.email); //* Undefined. Runtime error if not specified manuały
+
+    // persons["john"] = { displayName: "John Doe", emale: "john.doe@example.com" }; //* Typo Error
+    console.log("persons:", persons);
+  })();
+}

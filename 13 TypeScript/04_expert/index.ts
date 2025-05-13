a2179306197f6cfa954adace91ad67c47c50ab8b
Michal Radomski <michalra@op.pl>
2025-05-13 22:31:21 +0200

    TypeScript for Professionals course - commit 48

--- 13 TypeScript/04_expert/index.ts
+++ 13 TypeScript/04_expert/index.ts
@@ -212,3 +212,40 @@
     type Explanation4 = string;
   })();
 }
+
+{
+  //* Infer keyword and `ReturnType<T>`
+  ((): void => {
+    type IsArray<T> = T extends Array<infer Member> ? "array" : "other";
+
+    type WithArray = IsArray<string[]>;
+    type WithOther = IsArray<number>;
+
+    type UnboxArray<T> = T extends Array<infer Member> ? Member : T;
+    type UnboxedStringArray = UnboxArray<string[]>;
+    type UnboxedNumberArray = UnboxArray<number[]>;
+    type anythingElse = UnboxArray<string>;
+
+    // Use case: Generate the type on the fly when input of functionB depends on output of functionA
+    function createPerson(firstName: string, lastName: string) {
+      return {
+        firstName,
+        lastName,
+        fullName: `${firstName} ${lastName}`,
+      };
+    }
+
+    // type ReturnType<T> = T extends (...args: any) => infer R ? R : never;
+    type Person = ReturnType<typeof createPerson>;
+    //     type Person = {
+    //     firstName: string;
+    //     lastName: string;
+    //     fullName: string;
+    // }
+
+    // Inline
+    function logPerson(person: ReturnType<typeof createPerson>): void {
+      console.log("Person:", person.firstName, person.lastName, person.fullName);
+    }
+  })();
+}

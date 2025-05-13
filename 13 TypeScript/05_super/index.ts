0f0d6a9107aa30389f5e9d9b85656fcbcb4d2d4e
Michal Radomski <michalra@op.pl>
2025-05-13 23:03:06 +0200

    TypeScript for Professionals course - commit 55

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -50,3 +50,41 @@
     // email: "alice@example.com" // Error: Property 'email' does not exist on type 'UserWithoutEmail'
   };
 }
+
+{
+  //* Pick -> removes all but the specified keys from an object type.
+  interface Person {
+    name: string;
+    age: number;
+    location?: string;
+  }
+
+  const bob: Pick<Person, "name"> = {
+    name: "Bob",
+    // `Pick` has only kept name, so age and location were removed from the type and they can't be defined here
+  };
+}
+
+{
+  //* Exclude -> Exclude removes types from a union
+  type Primitive = string | number | boolean;
+  const value: Exclude<Primitive, string> = true; // a string cannot be used here since Exclude removed it from the type.
+}
+
+{
+  //* Parameters ->  extracts the parameter types of a function type as an array
+  // A sample function with two parameters
+  function greet(name: string, age: number): string {
+    return `Hello, my name is ${name} and I am ${age} years old.`;
+  }
+
+  // Extract the parameter types of the greet function as a tuple
+  type GreetParams = Parameters<typeof greet>; // [string, number]
+  //* type GreetParams = [name: string, age: number]
+
+  // Use the extracted parameter types to define an argument array
+  const args: GreetParams = ["Alice", 30];
+
+  // Call the function using the typed arguments
+  console.log(greet(...args)); // Output: Hello, my name is Alice and I am 30 years old.
+}

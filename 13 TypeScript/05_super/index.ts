1c3bade67d92d1620d72817980404ec7705ef068
Michal Radomski <michalra@op.pl>
2025-05-13 23:38:42 +0200

    TypeScript for Professionals course - commit 61

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -240,3 +240,32 @@
   const myCar = new Car();
   myCar.startEngine(); // Output: Car engine started with a roar!
 }
+
+{
+  //* Undefined vs Optional
+  ((): void => {
+    // Optional
+    type ExampleOptional = {
+      name?: string;
+    };
+    let optional: ExampleOptional;
+
+    optional = { name: undefined };
+    optional = {};
+
+    // Undefined
+    type ExampleUndefined = {
+      name: string | undefined;
+    };
+    let undef: ExampleUndefined;
+    undef = { name: undefined };
+    // undef = {} // Error: name is missing
+
+    // Use Optional unless you have the necessity of having a property with undefined value. E.g:
+    function setConfig(config: { name: string; priority?: number }) {}
+
+    setConfig({ name: "CaseA", priority: undefined });
+    setConfig({ name: "CaseB", priority: 1 });
+    setConfig({ name: "CaseC" });
+  })();
+}

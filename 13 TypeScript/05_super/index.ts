08822a38bbfb3e1d27b946bbce2912effe4a50fd
Michal Radomski <michalra@op.pl>
2025-05-13 22:58:07 +0200

    TypeScript for Professionals course - commit 54

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -25,3 +25,28 @@
     console.log(state.current); // { x: 0, y: 1 }
   })();
 }
+
+{
+  //* Omit
+  interface User {
+    id: string;
+    name: string;
+    email: string;
+    age: number;
+  }
+
+  // Create a new type by omitting the 'email' property from User
+  type UserWithoutEmail = Omit<User, "email">;
+  //   type UserWithoutEmail = {
+  //     id: string;
+  //     name: string;
+  //     age: number;
+  // }
+
+  const user: UserWithoutEmail = {
+    id: "123",
+    name: "Alice",
+    age: 30,
+    // email: "alice@example.com" // Error: Property 'email' does not exist on type 'UserWithoutEmail'
+  };
+}

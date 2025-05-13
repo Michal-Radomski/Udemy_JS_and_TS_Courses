a6026fc6461788762c6ad30c07d7570552631ba4
Michal Radomski <michalra@op.pl>
2025-05-13 23:33:09 +0200

    TypeScript for Professionals course - commit 60

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -222,3 +222,21 @@
     padding = ""; // Check autocomplete
   })();
 }
+
+{
+  //* Override
+  class Vehicle {
+    startEngine() {
+      console.log("Engine started");
+    }
+  }
+
+  class Car extends Vehicle {
+    override startEngine() {
+      console.log("Car engine started with a roar!");
+    }
+  }
+
+  const myCar = new Car();
+  myCar.startEngine(); // Output: Car engine started with a roar!
+}

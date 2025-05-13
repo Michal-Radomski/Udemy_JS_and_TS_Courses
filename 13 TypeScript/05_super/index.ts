092158d7b6fb5154e21917a76911c9a550d5e32a
Michal Radomski <michalra@op.pl>
2025-05-13 23:51:15 +0200

    TypeScript for Professionals course - commit 62

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -269,3 +269,43 @@
     setConfig({ name: "CaseC" });
   })();
 }
+
+{
+  //* Satisfies operator
+  ((): void => {
+    type Color = "red" | "green" | "blue";
+    type RGB = [red: number, green: number, blue: number];
+    type Palette = Record<Color, string | RGB>;
+
+    const palette = {
+      red: "red",
+      green: [0, 255, 0],
+      blue: "#0000ff",
+    } satisfies Palette;
+
+    const greenLevel = palette.green.at(1);
+    const redUpperCased = palette.red.toUpperCase();
+    const blueLevel = palette.blue.slice(-2);
+
+    console.log({ greenLevel }); // 255
+    console.log({ redUpperCased }); // RED
+    console.log({ blueLevel }); // ff
+
+    type personName = "John" | "Jack" | "Justin";
+    type otherDetails = {
+      id: number;
+      age: number;
+    };
+    type personInfo = personName | otherDetails;
+
+    type Person = {
+      myInfo: personInfo;
+      myOtherInfo: personInfo;
+    };
+
+    const applicant = {
+      myInfo: "John",
+      myOtherInfo: { id: 123, age: 22 },
+    } satisfies Person;
+  })();
+}

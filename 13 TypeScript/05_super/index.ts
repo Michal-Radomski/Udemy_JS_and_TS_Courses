a6027693440e0c4d55a5d5843f0672fe6ebe88ce
Michal Radomski <michalra@op.pl>
2025-05-13 23:04:57 +0200

    TypeScript for Professionals course - commit 566

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -88,3 +88,40 @@
   // Call the function using the typed arguments
   console.log(greet(...args)); // Output: Hello, my name is Alice and I am 30 years old.
 }
+
+{
+  //* Required<T>
+  (() => {
+    // Makes all properties in T required (already implemented by TypeScript)
+    //* type Required<T> = { [P in keyof T]-?: T[P] };
+
+    type PartialPoint = { x?: number; y?: number };
+    type Point = Required<PartialPoint>; // Same as { x: number, y: number }
+
+    // Use case
+    // Optional members for consumers
+    type CircleConfig = {
+      color?: string;
+      radius?: number;
+    };
+
+    class Circle {
+      // Required: all members will be present
+      private config: Required<CircleConfig>;
+      constructor(config: CircleConfig) {
+        this.config = {
+          color: config.color ?? "black",
+          radius: config.radius ?? 0,
+        };
+      }
+      draw() {
+        // No null check needed
+        console.log(`
+      Drawing a circle.
+      Color: ${this.config.color}
+      Radius: ${this.config.radius}
+      `);
+      }
+    }
+  })();
+}

b9d6058775470fbc0a0f481d887fe9ae23eb4f7a
Michal Radomski <michalra@op.pl>
2025-05-14 20:28:54 +0200

    TypeScript for Professionals course - commit 66

--- 13 TypeScript/05_super/index.ts
+++ 13 TypeScript/05_super/index.ts
@@ -428,3 +428,21 @@
     })();
   })();
 }
+
+{
+  //* String Manipulation Utilities
+  (): void => {
+    type TypeA = Uppercase<"type_a">; // TYPE_A
+    type TypeB = Lowercase<"TYPE_B">; // type_b
+    type TypeC = Capitalize<"type_c">; // Type_c
+    type TypeD = Uncapitalize<"Type_d">; // tYPE_D
+
+    // Use case (combined with template literal type)
+    type Getter<T extends string> = `get${Capitalize<T>}`;
+    type Setter<T extends string> = `set${Capitalize<T>}`;
+
+    type Name = "name";
+    type GetName = Getter<Name>;
+    type SetName = Setter<Name>;
+  };
+}

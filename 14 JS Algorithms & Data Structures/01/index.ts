97f8d223d29d555953bb12b35d9a5dd2fedefff4
Michal Radomski <michalra@op.pl>
2025-07-21 22:18:44 +0200

    JS Algorithms & Data Structures course - commit 0

--- 14 JS Algorithms & Data Structures/01/index.ts
+++ 14 JS Algorithms & Data Structures/01/index.ts
@@ -76,4 +76,33 @@
   }
 
   console.log(same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]));
+
+  function validAnagram(first: string, second: string): boolean {
+    if (first.length !== second.length) {
+      return false;
+    }
+
+    const lookup = {} as { [key: string]: number };
+
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+    }
+    console.log({ lookup });
+
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
+    }
+
+    return true;
+  }
+
+  console.log(validAnagram("cinema", "iceman"));
 }

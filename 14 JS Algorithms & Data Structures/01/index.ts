c716446d2d50e86de48335f2aee8b4ca6903b268
Michal Radomski <michalra@op.pl>
2025-07-21 23:29:17 +0200

    JS Algorithms & Data Structures course - commit 10

--- 14 JS Algorithms & Data Structures/01/index.ts
+++ 14 JS Algorithms & Data Structures/01/index.ts
@@ -270,3 +270,32 @@
   const array = [38, 27, 43, 3, 9, 82, 10];
   console.log(mergeSort(array)); // Output: [3, 9, 10, 27, 38, 43, 82]
 }
+
+{
+  //* Recursion
+  // Call Stack
+  // function takeShower(): string {
+  //   return "Showering!";
+  // }
+  // function eatBreakfast(): string {
+  //   let meal: string = cookFood();
+  //   return `Eating ${meal}`;
+  // }
+  // function cookFood(): string {
+  //   let items = ["Oatmeal", "Eggs", "Protein Shake"];
+  //   return items[Math.floor(Math.random() * items.length)];
+  // }
+  // function wakeUp(): void {
+  //   takeShower();
+  //   eatBreakfast();
+  //   console.log("Ok ready to go to work!");
+  // }
+  // wakeUp();
+
+  function factorial(n: number): number {
+    if (n === 0) return 1; // Base case
+    return n * factorial(n - 1); // Recursive case
+  }
+
+  console.log(factorial(5)); // Output: 120
+}

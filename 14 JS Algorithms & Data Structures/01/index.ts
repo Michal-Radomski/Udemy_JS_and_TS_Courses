9ccd43c239a984aa4f3fcdca8498f5439fee1bb5
Michal Radomski <michalra@op.pl>
2025-07-22 21:59:05 +0200

    JS Algorithms & Data Structures course - commit 13

--- 14 JS Algorithms & Data Structures/01/index.ts
+++ 14 JS Algorithms & Data Structures/01/index.ts
@@ -467,3 +467,110 @@
   }
   console.log(findLongestSubstring("rithmschool")); // 7
 }
+
+{
+  //* Divide and Conquer exercises
+  function countZeroes(arr: number[]): number {
+    // add whatever parameters you deem necessary - good luck!
+    let firstZero = findFirst(arr);
+    if (firstZero === -1) return 0;
+
+    return arr.length - firstZero;
+  }
+
+  function findFirst(arr: number[], low = 0, high = arr.length - 1): number {
+    if (high >= low) {
+      let mid = low + Math.floor((high - low) / 2);
+      if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+        return mid;
+      } else if (arr[mid] === 1) {
+        return findFirst(arr, mid + 1, high);
+      }
+      return findFirst(arr, low, mid - 1);
+    }
+    return -1;
+  }
+  console.log(countZeroes([1, 1, 1, 1, 0, 0])); // 2
+
+  {
+    function sortedFrequency(arr: number[], num: number): number {
+      let firstIdx = findFirst(arr, num);
+      if (firstIdx === -1) return firstIdx;
+      let lastIdx = findLast(arr, num);
+      return lastIdx - firstIdx + 1;
+    }
+
+    function findFirst(arr: number[], num: number, low = 0, high = arr.length - 1): number {
+      if (high >= low) {
+        let mid = Math.floor((low + high) / 2);
+        if ((mid === 0 || num > arr[mid - 1]) && arr[mid] === num) {
+          return mid;
+        } else if (num > arr[mid]) {
+          return findFirst(arr, num, mid + 1, high);
+        } else {
+          return findFirst(arr, num, low, mid - 1);
+        }
+      }
+      return -1;
+    }
+
+    function findLast(arr: number[], num: number, low = 0, high = arr.length - 1): number {
+      if (high >= low) {
+        let mid = Math.floor((low + high) / 2);
+        if ((mid === arr.length - 1 || num < arr[mid + 1]) && arr[mid] === num) {
+          return mid;
+        } else if (num < arr[mid]) {
+          return findLast(arr, num, low, mid - 1);
+        } else {
+          return findLast(arr, num, mid + 1, high);
+        }
+      }
+      return -1;
+    }
+    console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)); // 4
+  }
+
+  {
+    function findRotatedIndex(array: number[], num: number): number {
+      let pivot = findPivot(array) as number;
+      if (pivot > 0 && num >= array[0] && num <= array[pivot - 1]) {
+        return binarySearch(array, num, 0, pivot - 1);
+      } else {
+        return binarySearch(array, num, pivot, array.length - 1);
+      }
+    }
+
+    function binarySearch(array: number[], num: number, start = 0, end = array.length - 1): number {
+      if (array.length === 0) return -1;
+      if (num < array[start] || num > array[end]) return -1;
+
+      while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (array[mid] === num) {
+          return mid;
+        } else if (num < array[mid]) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+      return -1;
+    }
+
+    function findPivot(arr: number[]): number | undefined {
+      if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+      let start = 0;
+      let end = arr.length - 1;
+      while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (arr[mid] > arr[mid + 1]) return mid + 1;
+        else if (arr[start] <= arr[mid]) {
+          start = mid + 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+    console.log(findRotatedIndex([3, 4, 1, 2], 4)); // 1
+  }
+}

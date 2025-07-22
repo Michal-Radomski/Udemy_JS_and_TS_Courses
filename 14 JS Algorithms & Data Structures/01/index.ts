b9f3678f2c679774609baed8233e08b3594a8625
Michal Radomski <michalra@op.pl>
2025-07-22 21:51:08 +0200

    JS Algorithms & Data Structures course - commit 13

--- 14 JS Algorithms & Data Structures/01/index.ts
+++ 14 JS Algorithms & Data Structures/01/index.ts
@@ -399,3 +399,71 @@
   }
   console.log(findPair([6, 1, 4, 10, 2, 4], 2)); // true
 }
+
+{
+  //* Sliding Window Exercises
+  function maxSubarraySum(arr: number[], num: number): number {
+    if (arr.length < num) return null as any;
+
+    let total = 0;
+    for (let i = 0; i < num; i++) {
+      total += arr[i];
+    }
+    let currentTotal = total;
+    for (let i = num; i < arr.length; i++) {
+      currentTotal += arr[i] - arr[i - num];
+      total = Math.max(total, currentTotal);
+    }
+    return total;
+  }
+  console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+
+  function minSubArrayLen(nums: number[], sum: number): number {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while (start < nums.length) {
+      // if current window doesn't add up to the given sum then
+      // move the window to right
+      if (total < sum && end < nums.length) {
+        total += nums[end];
+        end++;
+      }
+      // if current window adds up to at least the sum given then
+      // we can shrink the window
+      else if (total >= sum) {
+        minLen = Math.min(minLen, end - start);
+        total -= nums[start];
+        start++;
+      }
+      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+      else {
+        break;
+      }
+    }
+
+    return minLen === Infinity ? 0 : minLen;
+  }
+  console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2
+
+  function findLongestSubstring(str: string): number {
+    let longest = 0;
+    let seen = {} as { [key: string]: number };
+    let start = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (seen[char]) {
+        start = Math.max(start, seen[char]);
+      }
+      // index - beginning of substring + 1 (to include current in count)
+      longest = Math.max(longest, i - start + 1);
+      // store the index of the next char so as to not double count
+      seen[char] = i + 1;
+    }
+    return longest;
+  }
+  console.log(findLongestSubstring("rithmschool")); // 7
+}

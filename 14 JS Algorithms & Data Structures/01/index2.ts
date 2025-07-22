1dca1dfdfcddc07af5d095d8eaaf4a1f06f90f86
Michal Radomski <michalra@op.pl>
2025-07-22 22:02:44 +0200

    JS Algorithms & Data Structures course - commit 14

--- 14 JS Algorithms & Data Structures/01/index2.ts
+++ 14 JS Algorithms & Data Structures/01/index2.ts
@@ -1,28 +1,49 @@
+//* Recursion
+// Call Stack
+// function takeShower(): string {
+//   return "Showering!";
+// }
+// function eatBreakfast(): string {
+//   let meal: string = cookFood();
+//   return `Eating ${meal}`;
+// }
+// function cookFood(): string {
+//   let items = ["Oatmeal", "Eggs", "Protein Shake"];
+//   return items[Math.floor(Math.random() * items.length)];
+// }
+// function wakeUp(): void {
+//   takeShower();
+//   eatBreakfast();
+//   console.log("Ok ready to go to work!");
+// }
+// wakeUp();
+
+function factorial(n: number): number {
+  if (n === 0) return 1; // Base case
+  return n * factorial(n - 1); // Recursive case
+}
+console.log(factorial(5)); // Output: 120
+
 {
-  //* Recursion
-  // Call Stack
-  // function takeShower(): string {
-  //   return "Showering!";
-  // }
-  // function eatBreakfast(): string {
-  //   let meal: string = cookFood();
-  //   return `Eating ${meal}`;
-  // }
-  // function cookFood(): string {
-  //   let items = ["Oatmeal", "Eggs", "Protein Shake"];
-  //   return items[Math.floor(Math.random() * items.length)];
-  // }
-  // function wakeUp(): void {
-  //   takeShower();
-  //   eatBreakfast();
-  //   console.log("Ok ready to go to work!");
-  // }
-  // wakeUp();
-
-  function factorial(n: number): number {
-    if (n === 0) return 1; // Base case
-    return n * factorial(n - 1); // Recursive case
+  // Recursive Version
+  function countDown(num: number): void {
+    if (num <= 0) {
+      console.log("All done!");
+      return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
   }
-
-  console.log(factorial(5)); // Output: 120
+  countDown(3);
+}
+{
+  // Iterative Version
+  function countDown(num: number) {
+    for (let i = num; i > 0; i--) {
+      console.log(i);
+    }
+    console.log("All done!");
+  }
+  countDown(3);
 }

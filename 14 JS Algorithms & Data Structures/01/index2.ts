e0623d5863f4648efecd882b75bd76d885a19a33
Michal Radomski <michalra@op.pl>
2025-07-22 22:43:25 +0200

    JS Algorithms & Data Structures course - commit 16

--- 14 JS Algorithms & Data Structures/01/index2.ts
+++ 14 JS Algorithms & Data Structures/01/index2.ts
@@ -69,3 +69,46 @@ function sumRange(num: number): number {
 }
 
 console.log(sumRange(4)); // 10
+
+{
+  //* Helper Method Recursion
+  function collectOddValues(arr: number[]): number[] {
+    let result = [] as number[];
+
+    function helper(helperInput: number[]): void {
+      if (helperInput.length === 0) {
+        return;
+      }
+
+      if (helperInput[0] % 2 !== 0) {
+        result.push(helperInput[0]);
+      }
+
+      helper(helperInput.slice(1));
+    }
+
+    helper(arr);
+
+    return result;
+  }
+
+  console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ 1, 3, 5, 7, 9 ]
+
+  //* Without helper method
+  function collectOddValues2(arr: number[]): number[] {
+    let newArr = [] as number[];
+
+    if (arr.length === 0) {
+      return newArr;
+    }
+
+    if (arr[0] % 2 !== 0) {
+      newArr.push(arr[0]);
+    }
+
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+  }
+
+  console.log(collectOddValues2([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ 1, 3, 5, 7, 9 ]
+}

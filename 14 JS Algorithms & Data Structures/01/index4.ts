095d143cda8ef1f61cd709532a80b5911bb869ec
Michal Radomski <michalra@op.pl>
2025-07-24 22:57:12 +0200

    JS Algorithms & Data Structures course - commit 25

--- 14 JS Algorithms & Data Structures/01/index4.ts
+++ 14 JS Algorithms & Data Structures/01/index4.ts
@@ -56,4 +56,43 @@ function bubbleSort2(arr: number[]): number[] {
 
 console.log(bubbleSort2([8, 1, 2, 3, 4, 5, 6, 7])); // [1, 2, 3, 4, 5, 6, 7, 8]
 
-//* Selection Sort
+//^ Selection Sort -> O(n^2)
+//* LEGACY VERSION (non ES2015 syntax)
+function selectionSort(arr: number[]): number[] {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      //SWAP!
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort2([0, 2, 34, 22, 10, 19, 17])); // [0,  2, 10, 17, 19, 22, 34]
+
+//* ES2015 VERSION
+function selectionSort2(arr: number[]): number[] {
+  const swap = (arr: number[], idx1: number, idx2: number) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
+
+console.log(selectionSort2([0, 2, 34, 22, 10, 19, 17])); // [0,  2, 10, 17, 19, 22, 34]

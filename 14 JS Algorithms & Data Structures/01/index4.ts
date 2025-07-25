42d1da27b7343f219b1786ea264c3d78aadcb0d7
Michal Radomski <michalra@op.pl>
2025-07-25 21:26:44 +0200

    JS Algorithms & Data Structures course - commit 26

--- 14 JS Algorithms & Data Structures/01/index4.ts
+++ 14 JS Algorithms & Data Structures/01/index4.ts
@@ -96,3 +96,20 @@ function selectionSort2(arr: number[]): number[] {
 }
 
 console.log(selectionSort2([0, 2, 34, 22, 10, 19, 17])); // [0,  2, 10, 17, 19, 22, 34]
+
+//^ Insertion Sort -> O(n^2)
+function insertionSort(arr: number[]): number[] {
+  let currentVal: number;
+  for (let i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+
+    let j: number;
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+console.log(insertionSort([2, 1, 9, 76, 4])); // [ 1, 2, 4, 9, 76 ]

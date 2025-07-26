23b3f2f1b868adee340b6c0e148563407d9c7a3f
Michal Radomski <michalra@op.pl>
2025-07-26 22:01:31 +0200

    JS Algorithms & Data Structures course - commit 30

--- 14 JS Algorithms & Data Structures/01/index5.ts
+++ 14 JS Algorithms & Data Structures/01/index5.ts
@@ -1,6 +1,7 @@
 //* Sorting Algorithms 2
 //- https://www.toptal.com/developers/sorting-algorithms
 //- https://www.bigocheatsheet.com
+//- https://en.wikipedia.org/wiki/Comparison_sort
 
 //^ Merge Sort -> O(n log n)
 // Merges two already sorted arrays
@@ -40,7 +41,7 @@ function mergeSort(arr: number[]): number[] {
 }
 console.log(mergeSort([10, 24, 76, 73])); // [ 10, 24, 73, 76 ]
 
-//^ Quick Sort
+//^ Quick Sort -> O(n log n) (average)
 //* Pivot
 // First Version
 function pivot1(arr: number[], start = 0, end = arr.length + 1): number {
@@ -89,7 +90,7 @@ console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3])); // 3
 
 function quickSort(arr: number[], left = 0, right = arr.length - 1): number[] {
   if (left < right) {
-    let pivotIndex = pivot(arr, left, right); // 3
+    let pivotIndex: number = pivot(arr, left, right);
     // Left
     quickSort(arr, left, pivotIndex - 1);
     // Right
@@ -99,3 +100,42 @@ function quickSort(arr: number[], left = 0, right = arr.length - 1): number[] {
 }
 
 console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23])); // [-3, 1, 2, 2,  3, 4, 5, 6, 9, 23, 100]
+
+//^ Radix Sort
+function getDigit(num: number, i: number): number {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+}
+
+function digitCount(num: number): number {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums: number[]): number {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+console.log(mostDigits([23, 567, 89, 12234324, 90])); // 8
+
+function radixSort(nums: number[]): number[] {
+  let maxDigitCount: number = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []) as number[][];
+    // console.log("digitBuckets:", digitBuckets);
+
+    for (let i = 0; i < nums.length; i++) {
+      let digit: number = getDigit(nums[i], k);
+      // console.log("digit:", digit);
+      digitBuckets[digit].push(nums[i]);
+    }
+    // console.log("digitBuckets:", digitBuckets);
+    nums = ([] as number[]).concat(...digitBuckets);
+  }
+  return nums;
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852])); // [ 12, 23, 345, 2345, 5467, 9852 ]

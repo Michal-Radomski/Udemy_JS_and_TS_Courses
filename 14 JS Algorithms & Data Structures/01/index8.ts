fcd135a284b9607b0de8fab4edc41e2a6ee642e0
Michal Radomski <michalra@op.pl>
2025-07-29 11:57:40 +0200

    JS Algorithms & Data Structures course - commit 46

--- 14 JS Algorithms & Data Structures/01/index8.ts
+++ 14 JS Algorithms & Data Structures/01/index8.ts
@@ -20,4 +20,26 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: string): this {
+    var newNode: Node = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+const list = new DoublyLinkedList();
+list.push("Test");
+list.push("HELLO");
+list.push("GOODBYE");
+list.push("!");
+
+console.log("list:", list);

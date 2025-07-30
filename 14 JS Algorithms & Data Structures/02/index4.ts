8f6e59b45cf58e9b41f0b125cdaacc72d38be5a1
Michal Radomski <michalra@op.pl>
2025-07-30 12:24:18 +0200

    JS Algorithms & Data Structures course - commit 53

---
+++ 14 JS Algorithms & Data Structures/02/index4.ts
@@ -0,0 +1,55 @@
+export {};
+//^ Stack
+
+class Node {
+  next: Node | null;
+  value: string;
+  constructor(value: string) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  first: Node | null;
+  last: Node | null;
+  size: number;
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val: string): number {
+    const newNode: Node = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop(): string | null {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const stack = new Stack();
+stack.push("Test");
+stack.push("HELLO");
+stack.push("GOODBYE");
+stack.push("!");
+stack.pop();
+
+console.log("stack:", stack);

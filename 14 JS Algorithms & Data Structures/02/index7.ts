96ef9217d469ee2c6e296535ae6de2a3bf636277
Michal Radomski <michalra@op.pl>
2025-08-01 15:41:17 +0200

    JS Algorithms & Data Structures course - commit 63

--- 14 JS Algorithms & Data Structures/02/index7.ts
+++ 14 JS Algorithms & Data Structures/02/index7.ts
@@ -1,1 +1,35 @@
 //^ Binary Heaps
+class MaxBinaryHeap {
+  values: number[];
+  constructor() {
+    this.values = [];
+  }
+
+  insert(element: number): void {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+
+  bubbleUp(): void {
+    let idx: number = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
+
+const heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+console.log("heap:", heap);

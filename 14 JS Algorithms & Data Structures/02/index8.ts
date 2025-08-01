516a9197104f76b5419ec63f6812cfd2ebaf6cf8
Michal Radomski <michalra@op.pl>
2025-08-01 18:06:49 +0200

    JS Algorithms & Data Structures course - commit 67

--- 14 JS Algorithms & Data Structures/02/index8.ts
+++ 14 JS Algorithms & Data Structures/02/index8.ts
@@ -1,1 +1,23 @@
 //^ Hash Tables
+// function hash(key: string, arrayLen: number): number {
+//   let total = 0;
+//   for (let char of key) {
+//     // map "a" to 1, "b" to 2, "c" to 3, etc.
+//     let value: number = char.charCodeAt(0) - 96;
+//     total = (total + value) % arrayLen;
+//   }
+//   return total;
+// }
+// console.log("hash('test', 10):", hash("test", 10));
+
+function hash(key: string, arrayLen: number): number {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
+console.log("hash('test', 10):", hash("test", 10));

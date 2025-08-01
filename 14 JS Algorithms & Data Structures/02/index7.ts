650d9a4c0eb7675473f3a2623231be9b5feb4d0b
Michal Radomski <michalra@op.pl>
2025-08-01 17:34:06 +0200

    JS Algorithms & Data Structures course - commit 64

--- 14 JS Algorithms & Data Structures/02/index7.ts
+++ 14 JS Algorithms & Data Structures/02/index7.ts
@@ -1,3 +1,4 @@
+export {};
 //^ Binary Heaps
 class MaxBinaryHeap {
   values: number[];
@@ -72,3 +73,92 @@ heap.insert(27);
 heap.insert(12);
 heap.insert(55);
 console.log("heap:", heap);
+heap.extractMax();
+console.log("heap:", heap);
+
+//-----------------
+class Node {
+  val: string;
+  priority: number;
+  constructor(val: string, priority: number) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  values: Node[];
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val: string, priority: number): void {
+    let newNode: Node = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp(): void {
+    let idx: number = this.values.length - 1;
+    const element: Node = this.values[idx];
+
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  dequeue(): Node {
+    const min: Node = this.values[0];
+    const end = this.values.pop() as Node;
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown(): void {
+    let idx = 0;
+    const length: number = this.values.length;
+    const element: Node = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild!.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+const ER = new PriorityQueue();
+ER.enqueue("common cold", 5);
+ER.enqueue("gunshot wound", 1);
+ER.enqueue("high fever", 4);
+ER.enqueue("broken arm", 2);
+ER.enqueue("glass in foot", 3);
+console.log("ER:", ER);
